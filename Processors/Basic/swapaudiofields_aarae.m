dd746759cf556bdc8ecbd0a0dc624dcc9718c09d
densil.cabrera@gmail.com <densil.cabrera@gmail.com@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-01-08 06:43:57 +0000

    included the case where audio3 exists but audio2 does not exist

--- Processors/Basic/swapaudiofields_aarae.m
+++ Processors/Basic/swapaudiofields_aarae.m
@@ -1,10 +1,15 @@
 function out = swapaudiofields_aarae(in)
 % The primary and secondary audio fields are swapped - i.e.,
-% .audio becomes .audio2, and .audio2 becomes .audio.
+% .audio becomes .audio2, and .audio2 becomes .audio. 
+% If .audio2 does not exist, but .audio3 exists, then it is swapped with
+% .audio instead.
 
     if isfield(in, 'audio2')
         out.audio = in.audio2;
         out.audio2 = in.audio;
+    elseif isfield(in, 'audio3')
+        out.audio = in.audio3;
+        out.audio3 = in.audio;
     else
         out = in;
         disp('Unable to swap audio fields because audio2 does not exist')
