5c55c9131219b3afc1c650d98c499c7818727c82
densil.cabrera@gmail.com <densil.cabrera@gmail.com@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-01-08 06:55:52 +0000

    ID field swap added

--- trunk/Processors/Basic/permute23_aarae.m
+++ trunk/Processors/Basic/permute23_aarae.m
@@ -1,5 +1,14 @@
 function out = permute23_aarae(in)
 % The 2nd and third dimensions of the input audio are swapped.
 % Hence channels become bands and bands become channels.
-    out = permute(in.audio,[1,3,2]);
+% ChanID and bandID fields are also swapped (if they exist).
+    out.audio = permute(in.audio,[1,3,2]);
+
+    if isfield(in,'bandID')
+        out.chanID = in.bandID;
+    end
+
+    if isfield(in,'chanID')
+        out.bandID = in.chanID;
+    end
 end
