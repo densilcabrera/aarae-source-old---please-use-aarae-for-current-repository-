d72ffc88b1d937ed13e189f776d1bb73d717d7c0
densil.cabrera@gmail.com <densil.cabrera@gmail.com@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-01-09 03:18:41 +0000

    extended functionality of swap audio fields - fields up to .audio16 can be dealt with

--- trunk/Processors/Basic/swapaudiofields_aarae.m
+++ trunk/Processors/Basic/swapaudiofields_aarae.m
@@ -1,17 +1,58 @@
 function out = swapaudiofields_aarae(in)
-% The primary and secondary audio fields are swapped - i.e.,
-% .audio becomes .audio2, and .audio2 becomes .audio. 
-% If .audio2 does not exist, but .audio3 exists, then it is swapped with
-% .audio instead.
+% If more than one audio field exists, then the field contents are swapped.
+% If more than two audio fields exist, then you can decide how the field
+% contents are moved via a dialog box.
 
-    if isfield(in, 'audio2')
-        out.audio = in.audio2;
-        out.audio2 = in.audio;
-    elseif isfield(in, 'audio3')
-        out.audio = in.audio3;
-        out.audio3 = in.audio;
-    else
-        out = in;
-        disp('Unable to swap audio fields because audio2 does not exist')
+
+% list of possible audio field names
+audiofieldnames = {'audio','audio2','audio3','audio4','audio5','audio6',...
+    'audio7','audio8','audio9','audio10','audio11','audio12','audio13',...
+    'audio14','audio15','audio16'};
+
+% check which audio fields exist
+audiofields = [isfield(in,audiofieldnames{1}), isfield(in,audiofieldnames{2}), ...
+    isfield(in,audiofieldnames{3}), isfield(in,audiofieldnames{4}), isfield(in,audiofieldnames{5}), ...
+    isfield(in,audiofieldnames{6}), isfield(in,audiofieldnames{7}), isfield(in,audiofieldnames{8}), ...
+    isfield(in,audiofieldnames{9}), isfield(in,audiofieldnames{10}), isfield(in,audiofieldnames{11}), ...
+    isfield(in,audiofieldnames{12}), isfield(in,audiofieldnames{13}), isfield(in,audiofieldnames{14}), ...
+    isfield(in,audiofieldnames{15}), isfield(in,audiofieldnames{16})];
+
+% remove fields that don't exist from the list of field names
+audiofieldnames = audiofieldnames(audiofields);
+
+out = in;
+
+if sum(audiofields) == 1
+    % if only one audio field exists, then swapping cannot be done
+    disp('Unable to swap audio fields because only one audio field exists')
+    
+elseif sum(audiofields) == 2
+    % if only two audio fields exist, then swap them without dialog
+    out.(audiofieldnames{1}) = in.(audiofieldnames{2});
+    out.(audiofieldnames{2}) = in.(audiofieldnames{1});
+    
+else
+    % if more than two audio fields exist, then ask the user how to swap them
+    
+    
+    
+    
+    
+    
+    param = inputdlg(audiofieldnames,...
+        'Swap audio fields',... % window title.
+        [1 60],...
+        cellstr(num2str(circshift((1:sum(audiofields))',1)))); % And the preset answers for your dialog.
+    
+    param = str2num(char(param));
+    
+    if length(param) < sum(audiofields), param = []; end
+    if ~isempty(param)
+        for n = 1:sum(audiofields)
+            out.(audiofieldnames{param(n)}) = in.(audiofieldnames{n});
+        end
     end
+    
+    
+    
 end
