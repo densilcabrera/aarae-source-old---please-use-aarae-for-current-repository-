2db0825dc7003bb240f081d871b21a6483ce48a9
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-02-09 23:48:36 +0000

    some minor tweaks

--- Utilities/lundebycrosspoint.m
+++ Utilities/lundebycrosspoint.m
@@ -21,7 +21,39 @@ function crosspoint = lundebycrosspoint(IR2, fs, fc)
 % curve fitting.
 %
 % Code by Densil Cabrera
-% version 0.00 (8 February 2014) - Beta
+% version 0.01 (10 February 2014) - Beta
+
+
+%**************************************************************************
+% Copyright (c) 2013, Densil Cabrera
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
+% met:
+%
+%  * Redistributions of source code must retain the above copyright notice,
+%    this list of conditions and the following disclaimer.
+%  * Redistributions in binary form must reproduce the above copyright
+%    notice, this list of conditions and the following disclaimer in the
+%    documentation and/or other materials provided with the distribution.
+%  * Neither the name of the University of Sydney nor the names of its contributors
+%    may be used to endorse or promote products derived from this software
+%    without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+% TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+% PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+% OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+% EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+% PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+% PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+% LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%**************************************************************************
+
 
 % if there are any negative values, assume that the IR has not been squared
 if min(min(min(IR2))) < 0
@@ -115,9 +147,9 @@ end
 
 % 6. AVERAGE SQUARED IR IN NEW LOCAL TIME INTERVALS
 for ch = 1:chans
-for b = 1:bands
-    IR2smooth(:,ch,b) = fftfilt(ones(winlen(1,ch,b),1)./winlen(1,ch,b),IR2(:,ch,b));
-end
+    for b = 1:bands
+        IR2smooth(:,ch,b) = fftfilt(ones(winlen(1,ch,b),1)./winlen(1,ch,b),IR2(:,ch,b));
+    end
 end
 IR2smoothdB =  10*log10(IR2smooth);
 maxIR2smoothdB = max(IR2smoothdB);
@@ -129,71 +161,73 @@ for ch = 1:chans
     end
 end
 
-
+ok = true;
 
 
 % ITERATE STEPS 7-9
 for iter = 1:5
-    
-    
-    % 7. ESTIMATE THE BACKGROUND NOISE LEVEL
-    noisefloorindex = zeros(1,chans,bands);
-    for ch = 1:chans
-        for b = 1:bands
-            noisefloorindex(1,ch,b) = round((o(2,ch,b)+IR2taildB(1,ch,b)-10)/o(1,ch,b));
-        end
-    end
-    noisefloorindex(noisefloorindex > round(0.9*len)) = round(0.9*len);
-    
-    for ch = 1:chans
-        for b = 1:bands
-            IR2tail(1,ch,b) = mean(IR2(noisefloorindex(1,ch,b):end,ch,b));
-            IR2taildB(1,ch,b) = 10*log10(IR2tail(1,ch,b))- maxIR2smoothdB(1,ch,b);
-        end
-    end
-    
-    
-    
-    % 8. ESTIMATE THE LATE DECAY SLOPE
-    for ch = 1:chans
-        for b = 1:bands
-            if IR2taildB(1,ch,b) < -35
-                LateSlopeEnddB = IR2taildB(1,ch,b) + 10;
-                LateSlopeStartdB = IR2taildB(1,ch,b) + 30;
-                ok = true;
-            elseif IR2taildB(1,ch,b) < -30
-                LateSlopeEnddB = IR2taildB(1,ch,b) + 8.5;
-                LateSlopeStartdB = IR2taildB(1,ch,b) + 25;
-                ok = true;
-            elseif IR2taildB(1,ch,b) < -25
-                LateSlopeEnddB = IR2taildB(1,ch,b) + 7;
-                LateSlopeStartdB = IR2taildB(1,ch,b) + 20;
-                ok = true;
-            elseif IR2taildB(1,ch,b) < -20
-                LateSlopeEnddB = IR2taildB(1,ch,b) + 6;
-                LateSlopeStartdB = IR2taildB(1,ch,b) + 16;
-                ok = true;
-            elseif IR2taildB(1,ch,b) < -15
-                LateSlopeEnddB = IR2taildB(1,ch,b) + 5;
-                LateSlopeStartdB = IR2taildB(1,ch,b) + 15;
-                ok = true;
-            else
-                ok = false;
+    if ok
+        
+        % 7. ESTIMATE THE BACKGROUND NOISE LEVEL
+        noisefloorindex = zeros(1,chans,bands);
+        for ch = 1:chans
+            for b = 1:bands
+                noisefloorindex(1,ch,b) = round((o(2,ch,b)+IR2taildB(1,ch,b)-10)/o(1,ch,b));
             end
-            if ok
-                tend = find(IR2smoothdB(maxind(1,ch,b):end,ch,b) <= LateSlopeEnddB, 1, 'first')+maxind(1,ch,b)-1; %
-                tstart = find(IR2smoothdB(maxind(1,ch,b):end,ch,b) <= LateSlopeStartdB, 1, 'first')+maxind(1,ch,b)-1; %
-                
-                o(:,ch,b) = polyfit((tstart:tend)', ...
-                    IR2smoothdB(tstart:tend,ch,b),1)';
-                
-                % 9. FIND NEW CROSSPOINT
-                crosspoint(1,ch,b) = -round((o(2,ch,b)-IR2taildB(1,ch,b))/o(1,ch,b));
+        end
+        noisefloorindex(noisefloorindex > round(0.9*len)) = round(0.9*len);
+        
+        for ch = 1:chans
+            for b = 1:bands
+                IR2tail(1,ch,b) = mean(IR2(noisefloorindex(1,ch,b):end,ch,b));
+                IR2taildB(1,ch,b) = 10*log10(IR2tail(1,ch,b))- maxIR2smoothdB(1,ch,b);
+            end
+        end
+        
+        
+        
+        % 8. ESTIMATE THE LATE DECAY SLOPE
+        for ch = 1:chans
+            for b = 1:bands
+                if IR2taildB(1,ch,b) < -35
+                    LateSlopeEnddB = IR2taildB(1,ch,b) + 10;
+                    LateSlopeStartdB = IR2taildB(1,ch,b) + 30;
+                elseif IR2taildB(1,ch,b) < -30
+                    LateSlopeEnddB = IR2taildB(1,ch,b) + 8.5;
+                    LateSlopeStartdB = IR2taildB(1,ch,b) + 25;
+                elseif IR2taildB(1,ch,b) < -25
+                    LateSlopeEnddB = IR2taildB(1,ch,b) + 7;
+                    LateSlopeStartdB = IR2taildB(1,ch,b) + 20;
+                elseif IR2taildB(1,ch,b) < -20
+                    LateSlopeEnddB = IR2taildB(1,ch,b) + 6;
+                    LateSlopeStartdB = IR2taildB(1,ch,b) + 16;
+                elseif IR2taildB(1,ch,b) < -15
+                    LateSlopeEnddB = IR2taildB(1,ch,b) + 5;
+                    LateSlopeStartdB = IR2taildB(1,ch,b) + 15;
+                else
+                    ok = false; % give up - insufficient SNR
+                end
+                if ok
+                    tend = find(IR2smoothdB(maxind(1,ch,b):end,ch,b) <= LateSlopeEnddB, 1, 'first')+maxind(1,ch,b)-1; %
+                    tstart = find(IR2smoothdB(maxind(1,ch,b):end,ch,b) <= LateSlopeStartdB, 1, 'first')+maxind(1,ch,b)-1; %
+                    
+                    o(:,ch,b) = polyfit((tstart:tend)', ...
+                        IR2smoothdB(tstart:tend,ch,b),1)';
+                    
+                    % 9. FIND NEW CROSSPOINT
+                    crosspoint(1,ch,b) = -round((o(2,ch,b)-IR2taildB(1,ch,b))/o(1,ch,b));
+                end
             end
         end
     end
-    
-    
-    
 end
 
+% DOUBLE-CHECK THAT CROSSPOINT IS REALLY OK
+for ch = 1:chans
+    for b = 1:bands
+        if crosspoint(1,ch,b) <= maxIR2smoothdB(1,ch,b)+0.05*fs || ... % at least 50 ms decay
+            crosspoint(1,ch,b) > len
+            crosspoint(1,ch,b) = len; % give up - no crosspoint found
+        end
+    end
+end
