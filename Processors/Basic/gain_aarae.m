56a2a6cc8ca42a1b34343130c50f0d05dcbe46d3
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-02-13 23:02:44 +0000

    fixed bug in gain_aarae, and added more capability (complex gain factor, standalone)

--- Processors/Basic/gain_aarae.m
+++ Processors/Basic/gain_aarae.m
@@ -1,30 +1,48 @@
-function out = gain_aarae(in)
+function out = gain_aarae(in,gain)
 % This function applies gain to the audio.
 % Gain is specified in dB, where:
 % 0 dB means no change
 % a positive value amplifies the waveform
 % a negative value attenuates the waveform
-% 
+%
 % The default value normalizes the audio, whilst preserving the amplitude
 % relationships between the channels and bands (where relevant)
 %
 % Inputting 'n' normalizes each data column individually
 %
+% If a complex valued gain is input, then it is used directly to multiply the
+% waveform (instead of converting it from decibels). A purely real argument
+% that includes 0i will be also be used to directly multiply the waveform.
+%
 % Code by Densil Cabrera
-% version 1.0 (14 October 2013)
+% version 1.01 (14 February 2013)
 
-Lmax = 20*log10(max(max(max(abs(in.audio)))));
-prompt = {['Gain (dB) or ''n'' (max is ',num2str(Lmax), ' dBFS)']};
-dlg_title = 'Gain';
-num_lines = 1;
-def = {num2str(-Lmax)};
-answer = inputdlg(prompt,dlg_title,num_lines,def);
+if isstruct(in)
+    Lmax = 20*log10(max(max(max(abs(in.audio)))));
+    prompt = {['Real gain (in dB), or ''n'' (max is ',num2str(Lmax),...
+        ' dBFS), or complex gain factor (not in dB)']};
+    dlg_title = 'Gain';
+    num_lines = 1;
+    def = {num2str(-Lmax)};
+    answer = inputdlg(prompt,dlg_title,num_lines,def);
+else
+    in.audio = in;
+    answer = gain;
+    if ~ischar(answer)
+        answer = num2str(answer);
+    end
+end
+
+
 if ~isempty(answer)
-    gain = answer{1,1};
-    if ischar(gain)
+    gain = char(answer{1,1});
+    if gain == 'n'
         % normalize column channel individually
         maxval = max(abs(in.audio));
         out = in.audio ./ repmat(maxval,[length(in.audio),1,1]);
+    elseif ~isempty(gain(gain == 'i'))
+        gain = str2num(gain);
+        out = in.audio * gain;
     else
         gain = str2num(gain);
         out = in.audio * 10.^(gain/20);
