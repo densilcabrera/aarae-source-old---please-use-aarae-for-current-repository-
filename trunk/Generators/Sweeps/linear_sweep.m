793d4433af9063a9e1c7f7e428c07833de1bc7cb
danieljimo@gmail.com <danieljimo@gmail.com@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-03-11 09:58:45 +0000

    * Fix to linear_sweep.m

--- trunk/Generators/Sweeps/linear_sweep.m
+++ trunk/Generators/Sweeps/linear_sweep.m
@@ -28,8 +28,17 @@ if ~isempty(param) || nargin ~=0
     if end_freq > fs/2, end_freq = fs/2; end
     
     t = 0:1/fs:(dur-1/fs); %time in seconds
-    S = chirp(t,start_freq,(dur-1/fs),end_freq)';
-    
+    %S = chirp(t,start_freq,(dur-1/fs),end_freq)';
+    t1 = dur-1/fs;
+    p = 1;
+    phi = 0;
+    rcos_ms = 15;
+    beta   = (end_freq-start_freq).*(t1.^(-p));
+    S = sin(2*pi * ( beta./(1+p).*(t.^(1+p)) + start_freq.*t + phi/360));
+    rcos_len = round(length(S)*((rcos_ms*1e-3)/dur));
+    sig_len = length(S);
+    rcoswin = hanning(2*rcos_len).';
+    S = [S(1:rcos_len).*rcoswin(1:rcos_len),S(rcos_len+1:sig_len-rcos_len),S(sig_len-rcos_len+1:sig_len).*rcoswin((rcos_len+1):(rcos_len*2))]';
     Sinv = flipud(S);
 
     OUT.audio = S;
