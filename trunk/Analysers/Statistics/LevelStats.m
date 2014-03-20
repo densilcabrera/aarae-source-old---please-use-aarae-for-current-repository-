4e98b52332723cf416f39c1b923a6d598d428ffe
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-03-20 11:09:32 +0000

    updates to Level stats - especially improvements to fig, and some bug fixes

--- trunk/Analysers/Statistics/LevelStats.m
+++ trunk/Analysers/Statistics/LevelStats.m
@@ -3,8 +3,8 @@ function out = LevelStats(inputwave)
 % calibration offset into account.
 %
 % code by Densil Cabrera
-% version 0 (12 March 2014)
-% plots and tables need more work!
+% version 1.00 (20 March 2014)
+
 if isstruct(inputwave)
     audio = inputwave.audio;
     fs = inputwave.fs;
@@ -13,22 +13,22 @@ if isstruct(inputwave)
     end
     if isfield(inputwave,'chanID')
         chanID = inputwave.chanID;
+        % not used yet...
     end
     if isfield(inputwave,'cal')
         cal = inputwave.cal;
     else
-        cal = 0;
+        cal = zeros(1,size(audio,2));
         disp('This audio signal has not been calibrated.')
     end
     
 end
 
-[len,chans,bands,dim4,dim5,dim6] = size(audio);
+[len,chans,bands] = size(audio);
 
 
 %dosubplots = 0; % default setting for multichannel plotting
 tau = 0.125; % default temporal integration constant in seconds
-showpercentiles = 1;
 weight = 'z';
 
 param = inputdlg({...
@@ -44,6 +44,9 @@ if ~isempty(param)
     cal = str2num(char(param(2)));
     weight = char(param(3));
 end
+
+% preserve the original data for plotting
+audio_original = audio .* repmat(10.^(cal./20),[len,1,bands]);
 
 % apply weighting
 audio = weighting(audio,fs,weight);
@@ -94,18 +97,41 @@ out.L50 = permute(out.L50,[3,2,1]);
 out.L90 = 10*log10(prctile(audio,10));
 out.L90 = permute(out.L90,[3,2,1]);
 
-ymax = 10*ceil(max(max(out.Lmax+5))/10);
-ymin = 10*floor(min(min(out.L90))/10)-20;
+ymax = 10*ceil(max(max(max(10*log10(audio_original.^2))))/10);
+ymin = 10*floor(min(min(min(out.L90(~isnan(out.L90)))))/10)-20;
 
 
 % Still need to write output tables and improve the figures
-
+t = ((1:len)'-1)./fs;
 for ch = 1:chans
     for b = 1:bands
-        figure('Name','Cumulative Distribution')
+        if bands > 1
+            if exist('bandID','var')
+                figure('Name',...
+                    ['Level Statistics, ch ',num2str(ch),', ',num2str(bandID(b))])
+            else
+                figure('Name',...
+                    ['Level Statistics, ch ',num2str(ch),', ',num2str(b)])
+                
+            end
+        else
+            figure('Name',['Level Statistics, ch ',num2str(ch)])
+        end
         y = sort(audio(:,ch,b));
-        plot(((1:len)'-1)./fs,10*log10(y))
+        plot(t,10*log10(audio_original(:,ch,b).^2),'Color',[0.7 0.9 0.9],...
+            'DisplayName','Raw audio')       
         hold on
+        plot(t,10*log10(audio(:,ch,b)),'Color',[0 0.5 0],...
+            'DisplayName','Processed audio')   
+        plot(t,10*log10(y),'r','DisplayName','Cumulative Distribution')
+        plot(t(round(length(t)*0.95)),10*log10(y(round(length(t)*0.95))),...
+            'r*','DisplayName',['L5: ',num2str(out.L5(b,ch)),' dB'])
+        plot(t(round(length(t)*0.9)),10*log10(y(round(length(t)*0.9))),...
+            'ro','DisplayName',['L10: ',num2str(out.L10(b,ch)),' dB'])
+        plot(t(round(length(t)*0.5)),10*log10(y(round(length(t)*0.5))),...
+            'r+','DisplayName',['L50: ',num2str(out.L50(b,ch)),' dB'])
+        plot(t(round(length(t)*0.1)),10*log10(y(round(length(t)*0.1))),...
+            'rx','DisplayName',['L90: ',num2str(out.L90(b,ch)),' dB'])
         ylabel('Level (dB)')
         xlabel('Duration (s)')
         ylim([ymin ymax])
@@ -113,5 +139,8 @@ for ch = 1:chans
             ['Leq = ',num2str(out.Leq(b,ch)),' dB'])
         text(0.7*(len-1)/fs,ymin+0.4*(ymax-ymin),...
             ['Lenergy = ',num2str(out.Lenergy(b,ch)),' dB'])
+        legend('show','Location','EastOutside');
+
+        
     end
 end
