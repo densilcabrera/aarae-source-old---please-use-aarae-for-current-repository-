051ee15f7f2758e97419424e3d0e51a7d4100d75
danieljimo@gmail.com <danieljimo@gmail.com@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-03-25 02:00:48 +0000

    * Enhancement of functions in the Energy ratios/ folder to comply with batch analyzing.

--- trunk/Analysers/Energy ratios/CentreTime_varTlim.m
+++ trunk/Analysers/Energy ratios/CentreTime_varTlim.m
@@ -10,10 +10,12 @@ function out = CentreTime_varTlim(data, fs, startthresh, highestband, lowestband
 % Code by Densil Cabrera and Grant Cuthbert
 % version 1.0 (15 December 2013)
 
-if isstruct(data)
-    IR = data.audio;
-    fs = data.fs;
-    
+if nargin < 7, doplot = 1; end
+if nargin < 6, bpo = 1; end
+if nargin < 5, lowestband = 125; end
+if nargin < 4, highestband = 4000; end
+if nargin < 3
+    startthresh = -20;
     %dialog box for settings
     prompt = {'Threshold for IR start detection', ...
         'Bands per octave (1 | 3)', ...
@@ -32,158 +34,163 @@ if isstruct(data)
         lowestband  = str2num(answer{4,1});
         doplot = str2num(answer{5,1});
     end
-    
+end
+if isstruct(data)
+    IR = data.audio;
+    fs = data.fs;
 else
-    
     IR = data;
-    if nargin < 7, doplot = 1; end
-    if nargin < 6, bpo = 1; end
-    if nargin < 5, lowestband = 125; end
-    if nargin < 4, highestband = 4000; end
-    if nargin < 3, startthresh = -20; end
-    
-end
-
-%--------------------------------------------------------------------------
-% TRUNCATION
-%--------------------------------------------------------------------------
-
-% Check the input data dimensions
-[len, chans, bands] = size(IR);
-if bands > 1
-    if isfield(in,'bandID')
-        flist = in.bandID;
-    else
-        flist = 1:bands;
+    if nargin < 2
+        fs = inputdlg({'Sampling frequency [samples/s]'},...
+                           'Fs',1,{'48000'});
+        fs = str2num(char(fs));
     end
 end
 
+if ~isempty(IR) && ~isempty(fs) && ~isempty(startthresh) && ~isempty(highestband) && ~isempty(lowestband) && ~isempty(bpo) && ~isempty(doplot)
+    %--------------------------------------------------------------------------
+    % TRUNCATION
+    %--------------------------------------------------------------------------
 
-% Preallocate
-m = zeros(1, chans); % maximum value of the IR
-startpoint = zeros(1, chans); % the auto-detected start time of the IR
-
-for dim2 = 1:chans
-    m(1,dim2) = max(IR(:,dim2).^2); % maximum value of the IR
-    startpoint(1,dim2) = find(IR(:,dim2).^2 >= m(1,dim2)./ ...
-        (10^(abs(startthresh)/10)),1,'first'); % Define start point
-    
-    if startpoint(1,dim2) > 1
-        % zero the data before the startpoint
-        IR(1:startpoint(1,dim2)-1,dim2) = 0;
-        
-        % rotate the zeros to the end (to keep a constant data length)
-        IR(:,dim2) = circshift(IR(:,dim2),-(startpoint(1,dim2)-1));
-    end
-end
-
-
-%--------------------------------------------------------------------------
-% FILTER AND CALCULATE CENTRE TIME
-%--------------------------------------------------------------------------
-
-
-% determine the filter frequencies
-if bands == 1
-    hiband = round(10*log10(highestband));
-    if hiband > 42, hiband = 42; end
-    loband = round(10*log10(lowestband));
-    if loband <15, loband = 15; end
-    if hiband > loband
-        if bpo == 3
-            flist = 10.^((loband:hiband)./10);
+    % Check the input data dimensions
+    [len, chans, bands] = size(IR);
+    if bands > 1
+        if isfield(in,'bandID')
+            flist = in.bandID;
         else
-            flist = 10.^((loband:3:hiband)./10);
+            flist = 1:bands;
+        end
+    end
+
+
+    % Preallocate
+    m = zeros(1, chans); % maximum value of the IR
+    startpoint = zeros(1, chans); % the auto-detected start time of the IR
+
+    for dim2 = 1:chans
+        m(1,dim2) = max(IR(:,dim2).^2); % maximum value of the IR
+        startpoint(1,dim2) = find(IR(:,dim2).^2 >= m(1,dim2)./ ...
+            (10^(abs(startthresh)/10)),1,'first'); % Define start point
+
+        if startpoint(1,dim2) > 1
+            % zero the data before the startpoint
+            IR(1:startpoint(1,dim2)-1,dim2) = 0;
+
+            % rotate the zeros to the end (to keep a constant data length)
+            IR(:,dim2) = circshift(IR(:,dim2),-(startpoint(1,dim2)-1));
+        end
+    end
+
+
+    %--------------------------------------------------------------------------
+    % FILTER AND CALCULATE CENTRE TIME
+    %--------------------------------------------------------------------------
+
+
+    % determine the filter frequencies
+    if bands == 1
+        hiband = round(10*log10(highestband));
+        if hiband > 42, hiband = 42; end
+        loband = round(10*log10(lowestband));
+        if loband <15, loband = 15; end
+        if hiband > loband
+            if bpo == 3
+                flist = 10.^((loband:hiband)./10);
+            else
+                flist = 10.^((loband:3:hiband)./10);
+            end
+        else
+            flist = 10.^(hiband./10);
+        end
+        % convert exact frequencies to nominal
+        flist = exact2nom_oct(flist);
+    end
+
+
+
+
+    % filter and square
+    if bands ==1
+        bands = length(flist);
+        if bpo ==3
+            IR2 = thirdoctbandfilter(IR,fs,flist).^2;
+        else
+            IR2 = octbandfilter(IR,fs,flist).^2;
         end
     else
-        flist = 10.^(hiband./10);
+        IR2 = IR.^2;
     end
-    % convert exact frequencies to nominal
-    flist = exact2nom_oct(flist);
-end
+
+    IR2xt = IR2 .* repmat(((0:len-1)./fs)', [1,chans, bands]);
 
 
+    %--------------------------------------------------------------------------
+    % CALCULATE CENTRE TIME
+    %--------------------------------------------------------------------------
+
+    % List of integration limits
+    tlim = 0.01:0.005:len/fs;
+
+    Ts = zeros(length(tlim),chans,bands);
+    for tcount = 1:length(tlim)
+
+        Ts(tcount,:,:) = 1000 .* sum(IR2xt(1:floor(tlim(tcount).*fs),:,:),1) ...
+            ./ sum(IR2(1:floor(tlim(tcount).*fs),:,:),1);
+    end
 
 
-% filter and square
-if bands ==1
-    bands = length(flist);
-    if bpo ==3
-        IR2 = thirdoctbandfilter(IR,fs,flist).^2;
-    else
-        IR2 = octbandfilter(IR,fs,flist).^2;
+    %--------------------------------------------------------------------------
+    % OUTPUT STRUCTURE
+    %--------------------------------------------------------------------------
+
+    out.Ts = Ts;
+    out.CentreFrequencies = flist;
+    out.TimeLimits = tlim;
+    out.Tsmax = squeeze(Ts(length(tlim),:,:));
+    out.funcallback.name = 'CentreTime_varTlim.m';
+    out.funcallback.inarg = {fs,startthresh,highestband,lowestband,bpo,doplot};
+
+    %--------------------------------------------------------------------------
+    % FIGURES
+    %--------------------------------------------------------------------------
+
+
+    [r, c] = subplotpositions(bands, 0.5);
+
+    for ch = 1:chans
+
+        figure('Name',['Centre time vs end truncation, channel ', num2str(ch)]);
+
+        for b = 1:bands
+            subplot(r,c,b)
+            plot(tlim, Ts(:,ch,b),'Color',[0.5,0,0])
+            title([num2str(flist(b)), ' Hz, ts = ', ...
+                num2str(round(Ts(length(tlim),ch,b))), ' ms'])
+            xlim([0 max(tlim)])
+            xlabel('End Truncation Time (s)')
+            ylabel('Centre Time (ms)')
+            hold on
+            plot([0, tlim(end)], ...
+                [Ts(length(tlim),ch,b), Ts(length(tlim),ch,b)], ...
+                'Color',[0.8,0,0], 'LineStyle', ':')
+
+        end
+
+        if doplot == 1
+            % Table
+            fig1 = figure('Name',['Centre Time ', num2str(ch)]);
+            table1 = uitable('Data',permute(Ts(:,ch,:),[1,3,2]),...
+                'ColumnName',num2cell(flist),...
+                'RowName',num2cell(tlim*1000));
+            set(table1,'ColumnWidth',{60});
+
+            disptables(fig1,table1 );
+        end
     end
 else
-    IR2 = IR.^2;
+    out = [];
 end
-
-IR2xt = IR2 .* repmat(((0:len-1)./fs)', [1,chans, bands]);
-
-
-%--------------------------------------------------------------------------
-% CALCULATE CENTRE TIME
-%--------------------------------------------------------------------------
-
-% List of integration limits
-tlim = 0.01:0.005:len/fs;
-
-Ts = zeros(length(tlim),chans,bands);
-for tcount = 1:length(tlim)
-    
-    Ts(tcount,:,:) = 1000 .* sum(IR2xt(1:floor(tlim(tcount).*fs),:,:),1) ...
-        ./ sum(IR2(1:floor(tlim(tcount).*fs),:,:),1);
-end
-
-
-%--------------------------------------------------------------------------
-% OUTPUT STRUCTURE
-%--------------------------------------------------------------------------
-
-out.Ts = Ts;
-out.CentreFrequencies = flist;
-out.TimeLimits = tlim;
-out.Tsmax = squeeze(Ts(length(tlim),:,:));
-
-%--------------------------------------------------------------------------
-% FIGURES
-%--------------------------------------------------------------------------
-
-
-[r, c] = subplotpositions(bands, 0.5);
-
-for ch = 1:chans
-    
-    figure('Name',['Centre time vs end truncation, channel ', num2str(ch)]);
-    
-    for b = 1:bands
-        subplot(r,c,b)
-        plot(tlim, Ts(:,ch,b),'Color',[0.5,0,0])
-        title([num2str(flist(b)), ' Hz, ts = ', ...
-            num2str(round(Ts(length(tlim),ch,b))), ' ms'])
-        xlim([0 max(tlim)])
-        xlabel('End Truncation Time (s)')
-        ylabel('Centre Time (ms)')
-        hold on
-        plot([0, tlim(end)], ...
-            [Ts(length(tlim),ch,b), Ts(length(tlim),ch,b)], ...
-            'Color',[0.8,0,0], 'LineStyle', ':')
-        
-    end
-    
-    if doplot == 1
-        % Table
-        fig1 = figure('Name',['Centre Time ', num2str(ch)]);
-        table1 = uitable('Data',permute(Ts(:,ch,:),[1,3,2]),...
-            'ColumnName',num2cell(flist),...
-            'RowName',num2cell(tlim*1000));
-        set(table1,'ColumnWidth',{60});
-
-        disptables(fig1,table1 );
-    end
-    
-end
-
-end % eof
+% eof
 
 %**************************************************************************
 % Copyright (c) 2013, Densil Cabrera and Grant Cuthbert
--- trunk/Analysers/Energy ratios/ClarityandDefinition_varTlim.m
+++ trunk/Analysers/Energy ratios/ClarityandDefinition_varTlim.m
@@ -1,4 +1,4 @@
-function out = ClarityandDefinition_varTlim(data, fs, startthresh, highestband, lowestband, bpo, maxtlim, doplot,endtime)
+function out = ClarityandDefinition_varTlim(data, fs, startthresh, highestband, lowestband, bpo, maxtlim, doplot, endtime)
 % This function calculates Clarity Index and Definition from an impulse
 % response, using a range of integration times (from 10 ms to 100 ms, in 
 % 5 ms steps).
@@ -30,10 +30,13 @@ function out = ClarityandDefinition_varTlim(data, fs, startthresh, highestband, 
 % Code by Densil Cabrera and Grant Cuthbert
 % version 1.0 (15 December 2013)
 
-if isstruct(data)
-    IR = data.audio;
-    fs = data.fs;
-    
+if nargin < 8, endtime = 60; end
+if nargin < 7, doplot = 1; end
+if nargin < 6, bpo = 1; end
+if nargin < 5, lowestband = 125; end
+if nargin < 4, highestband = 4000; end
+if nargin < 3
+    startthresh = -20;
     %dialog box for settings
     prompt = {'Threshold for IR start detection', ...
         'Bands per octave (1 | 3)', ...
@@ -56,191 +59,196 @@ if isstruct(data)
         maxtlim = str2num(answer{6,1});
         doplot = str2num(answer{7,1});
     end
-    
+end
+if isstruct(data)
+    IR = data.audio;
+    fs = data.fs;
 else
-    
     IR = data;
-    if nargin < 8, endtime = 60; end
-    if nargin < 7, doplot = 1; end
-    if nargin < 6, bpo = 1; end
-    if nargin < 5, lowestband = 125; end
-    if nargin < 4, highestband = 4000; end
-    if nargin < 3, startthresh = -20; end
-    
-end
-
-%--------------------------------------------------------------------------
-% TRUNCATION
-%--------------------------------------------------------------------------
-
-% Check the input data dimensions
-[len, chans, bands] = size(IR);
-if bands > 1
-    error('Input IR cannot be multiband');
-end
-
-
-% Preallocate
-m = zeros(1, chans); % maximum value of the IR
-startpoint = zeros(1, chans); % the auto-detected start time of the IR
-
-for dim2 = 1:chans
-    m(1,dim2) = max(IR(:,dim2).^2); % maximum value of the IR
-    startpoint(1,dim2) = find(IR(:,dim2).^2 >= m(1,dim2)./ ...
-        (10^(abs(startthresh)/10)),1,'first'); % Define start point
-    
-    if startpoint(1,dim2) > 1
-        % zero the data before the startpoint
-        IR(1:startpoint(1,dim2)-1,dim2) = 0;
-        
-        % rotate the zeros to the end (to keep a constant data length)
-        IR(:,dim2) = circshift(IR(:,dim2),-(startpoint(1,dim2)-1));
+    if nargin < 2
+        fs = inputdlg({'Sampling frequency [samples/s]'},...
+                           'Fs',1,{'48000'});
+        fs = str2num(char(fs));
     end
 end
 
-% End truncation
-if round(endtime*fs) < len
-    IR = IR(1:round(endtime*fs),:);
-end
+if ~isempty(IR) && ~isempty(fs) && ~isempty(startthresh) && ~isempty(highestband) && ~isempty(lowestband) && ~isempty(bpo) && ~isempty(maxtlim) && ~isempty(doplot) && ~isempty(endtime)
+    %--------------------------------------------------------------------------
+    % TRUNCATION
+    %--------------------------------------------------------------------------
+
+    % Check the input data dimensions
+    [len, chans, bands] = size(IR);
+    if bands > 1
+        error('Input IR cannot be multiband');
+    end
 
 
-hiband = round(10*log10(highestband));
-if hiband > 42, hiband = 42; end
-loband = round(10*log10(lowestband));
-if loband <15, loband = 15; end
-if hiband > loband
-    if bpo == 3
-        flist = 10.^((loband:hiband)./10);
+    % Preallocate
+    m = zeros(1, chans); % maximum value of the IR
+    startpoint = zeros(1, chans); % the auto-detected start time of the IR
+
+    for dim2 = 1:chans
+        m(1,dim2) = max(IR(:,dim2).^2); % maximum value of the IR
+        startpoint(1,dim2) = find(IR(:,dim2).^2 >= m(1,dim2)./ ...
+            (10^(abs(startthresh)/10)),1,'first'); % Define start point
+
+        if startpoint(1,dim2) > 1
+            % zero the data before the startpoint
+            IR(1:startpoint(1,dim2)-1,dim2) = 0;
+
+            % rotate the zeros to the end (to keep a constant data length)
+            IR(:,dim2) = circshift(IR(:,dim2),-(startpoint(1,dim2)-1));
+        end
+    end
+
+    % End truncation
+    if round(endtime*fs) < len
+        IR = IR(1:round(endtime*fs),:);
+    end
+
+
+    hiband = round(10*log10(highestband));
+    if hiband > 42, hiband = 42; end
+    loband = round(10*log10(lowestband));
+    if loband <15, loband = 15; end
+    if hiband > loband
+        if bpo == 3
+            flist = 10.^((loband:hiband)./10);
+        else
+            flist = 10.^((loband:3:hiband)./10);
+        end
     else
-        flist = 10.^((loband:3:hiband)./10);
+        flist = 10.^(hiband./10);
+    end
+    % convert exact frequencies to nominal
+    flist = exact2nom_oct(flist);
+
+
+    %--------------------------------------------------------------------------
+    % TRUNCATE, FILTER, SQUARE, SUM, AND CALCULATE CLARITY INDEX
+    %--------------------------------------------------------------------------
+
+    % List of temporal energy summation limits
+    if maxtlim < 0.08, maxtlim = 0.08; end
+    tlim = 0.01:0.005:maxtlim;
+    index35 = find(tlim == 0.035, 1, 'first');
+    index50 = find(tlim == 0.05, 1, 'first');
+    index80 = find(tlim == 0.08, 1, 'first');
+
+    % Pre-allocate
+    ClarityIndex = zeros(length(tlim),chans,length(flist));
+
+    % some zero-padding is applied to early, below, to avoid cutting off the
+    % filters' decays
+    for tcount = 1:length(tlim)
+        if bpo == 3
+            Early = sum(thirdoctbandfilter([IR(1:1+floor(fs*tlim(tcount)),:); ...
+                zeros(round(fs/50),chans)],fs,flist).^2);
+            Late = sum(thirdoctbandfilter(IR(ceil(fs*tlim(tcount)):end,:), ...
+                fs,flist).^2);
+        else
+            Early = sum(octbandfilter([IR(1:1+floor(fs*tlim(tcount)),:); ...
+                zeros(round(fs/50),chans)],fs,flist).^2);
+            Late = sum(octbandfilter(IR(ceil(fs*tlim(tcount)):end,:), ...
+                fs,flist).^2);
+        end
+        ClarityIndex(tcount,:,:) = 10*log10(Early./Late);
+    end
+    Definition = 10.^(ClarityIndex./10)./(10.^(ClarityIndex./10)+1);
+
+    %--------------------------------------------------------------------------
+    % OUTPUT STRUCTURE
+    %--------------------------------------------------------------------------
+
+    out.ClarityIndex = ClarityIndex;
+    out.Definition = Definition;
+    out.CentreFrequencies = flist;
+    out.TimeLimits = tlim;
+    out.C50 = ClarityIndex(index50,:,:);
+    out.C80 = ClarityIndex(index80,:,:);
+    out.funcallback.name = 'ClarityandDefinition_varTlim.m';
+    out.funcallback.inarg = {fs,startthresh,highestband,lowestband,bpo,maxtlim,doplot,endtime};
+
+    %--------------------------------------------------------------------------
+    % FIGURES
+    %--------------------------------------------------------------------------
+
+    if doplot == 1
+        bands = length(flist);
+        [r, c] = subplotpositions(bands, 0.5);
+
+        for ch = 1:chans
+
+            figure('Name',['Clarity Index, channel ', num2str(ch)]);
+
+            for b = 1:bands
+                subplot(r,c,b)
+                plot(tlim*1000, ClarityIndex(:,ch,b),'Color',[0.5,0,0])
+                title([num2str(flist(b)), ' Hz'])
+                xlim([0 max(tlim)*1000])
+                xlabel('Integration Limit (ms)')
+                ylabel('Clarity Index (dB)')
+                hold on
+                plot(50,ClarityIndex(index50,ch,b),'ro')
+                hold on
+                plot([0 50],[ClarityIndex(index50,ch,b), ...
+                    ClarityIndex(index50,ch,b)],':','Color', [1,0.8,0.8])
+                plot(80,ClarityIndex(index80,ch,b),'ro')
+                hold on
+                plot([0 80],[ClarityIndex(index80,ch,b), ...
+                    ClarityIndex(index80,ch,b)],':','Color', [1,0.8,0.8])
+                text(10,ClarityIndex(index50,ch,b),...
+                    [num2str(round(ClarityIndex(index50,ch,b)*10)/10), ' dB'])
+                text(10,ClarityIndex(index80,ch,b),...
+                    [num2str(round(ClarityIndex(index80,ch,b)*10)/10), ' dB'])
+                hold off
+
+            end
+
+                % Table
+        fig1 = figure('Name',['Clarity Index & Definition, channel ', num2str(ch)]);
+        table1 = uitable('Data',permute(ClarityIndex(:,ch,:),[1,3,2]),...
+                    'ColumnName',num2cell(flist),...
+                    'RowName',num2cell(tlim*1000));
+                set(table1,'ColumnWidth',{60});
+
+                table2 = uitable('Data',permute(Definition(:,ch,:),[1,3,2]),...
+                    'ColumnName',num2cell(flist),...
+                    'RowName',num2cell(tlim*1000));
+                set(table2,'ColumnWidth',{60});
+
+        disptables(fig1,[table1 table2]);
+        end
+        if chans == 1
+            figure('Name', 'Clarity Index')
+        else
+            figure('Name', 'Clarity Index Average of All Channels')
+        end
+
+        meanClarity = 10*log10(mean(10.^(ClarityIndex./10),2));
+        meanClarity = permute(meanClarity,[1,3,2]);
+        All = [meanClarity(1:index35-1,:); ...
+            meanClarity(index35+1:index50-1,:); ...
+            meanClarity(index50+1:index80-1,:); ...
+            meanClarity(index80+1:end,:)];
+
+        semilogx(flist,All,'Color',[0.8 0.8 0.8])
+        hold on
+        semilogx(flist,meanClarity(index35,:),'-ro','LineWidth',1,'MarkerSize',8); 
+        semilogx(flist,meanClarity(index50,:),'-r+','LineWidth',1,'MarkerSize',8);
+        semilogx(flist,meanClarity(index80,:),'-rx','LineWidth',1,'MarkerSize',8);
+        grid on
+        xlabel('Band Frequency (Hz)');
+        ylabel('Clarity Index (dB)');
+        xlim([flist(1) flist(end)])
+        title('Clarity index, 10-100 ms time limits (o C35; + C50; x C80)')
+        %legend('show','Location','EastOutside');
+        hold off
     end
 else
-    flist = 10.^(hiband./10);
+    out = [];
 end
-% convert exact frequencies to nominal
-flist = exact2nom_oct(flist);
-
-
-%--------------------------------------------------------------------------
-% TRUNCATE, FILTER, SQUARE, SUM, AND CALCULATE CLARITY INDEX
-%--------------------------------------------------------------------------
-
-% List of temporal energy summation limits
-if maxtlim < 0.08, maxtlim = 0.08; end
-tlim = 0.01:0.005:maxtlim;
-index35 = find(tlim == 0.035, 1, 'first');
-index50 = find(tlim == 0.05, 1, 'first');
-index80 = find(tlim == 0.08, 1, 'first');
-
-% Pre-allocate
-ClarityIndex = zeros(length(tlim),chans,length(flist));
-
-% some zero-padding is applied to early, below, to avoid cutting off the
-% filters' decays
-for tcount = 1:length(tlim)
-    if bpo == 3
-        Early = sum(thirdoctbandfilter([IR(1:1+floor(fs*tlim(tcount)),:); ...
-            zeros(round(fs/50),chans)],fs,flist).^2);
-        Late = sum(thirdoctbandfilter(IR(ceil(fs*tlim(tcount)):end,:), ...
-            fs,flist).^2);
-    else
-        Early = sum(octbandfilter([IR(1:1+floor(fs*tlim(tcount)),:); ...
-            zeros(round(fs/50),chans)],fs,flist).^2);
-        Late = sum(octbandfilter(IR(ceil(fs*tlim(tcount)):end,:), ...
-            fs,flist).^2);
-    end
-    ClarityIndex(tcount,:,:) = 10*log10(Early./Late);
-end
-Definition = 10.^(ClarityIndex./10)./(10.^(ClarityIndex./10)+1);
-
-%--------------------------------------------------------------------------
-% OUTPUT STRUCTURE
-%--------------------------------------------------------------------------
-
-out.ClarityIndex = ClarityIndex;
-out.Definition = Definition;
-out.CentreFrequencies = flist;
-out.TimeLimits = tlim;
-out.C50 = ClarityIndex(index50,:,:);
-out.C80 = ClarityIndex(index80,:,:);
-
-%--------------------------------------------------------------------------
-% FIGURES
-%--------------------------------------------------------------------------
-
-if doplot == 1
-    bands = length(flist);
-    [r, c] = subplotpositions(bands, 0.5);
-    
-    for ch = 1:chans
-        
-        figure('Name',['Clarity Index, channel ', num2str(ch)]);
-        
-        for b = 1:bands
-            subplot(r,c,b)
-            plot(tlim*1000, ClarityIndex(:,ch,b),'Color',[0.5,0,0])
-            title([num2str(flist(b)), ' Hz'])
-            xlim([0 max(tlim)*1000])
-            xlabel('Integration Limit (ms)')
-            ylabel('Clarity Index (dB)')
-            hold on
-            plot(50,ClarityIndex(index50,ch,b),'ro')
-            hold on
-            plot([0 50],[ClarityIndex(index50,ch,b), ...
-                ClarityIndex(index50,ch,b)],':','Color', [1,0.8,0.8])
-            plot(80,ClarityIndex(index80,ch,b),'ro')
-            hold on
-            plot([0 80],[ClarityIndex(index80,ch,b), ...
-                ClarityIndex(index80,ch,b)],':','Color', [1,0.8,0.8])
-            text(10,ClarityIndex(index50,ch,b),...
-                [num2str(round(ClarityIndex(index50,ch,b)*10)/10), ' dB'])
-            text(10,ClarityIndex(index80,ch,b),...
-                [num2str(round(ClarityIndex(index80,ch,b)*10)/10), ' dB'])
-            hold off
-            
-        end
-        
-            % Table
-    fig1 = figure('Name',['Clarity Index & Definition, channel ', num2str(ch)]);
-    table1 = uitable('Data',permute(ClarityIndex(:,ch,:),[1,3,2]),...
-                'ColumnName',num2cell(flist),...
-                'RowName',num2cell(tlim*1000));
-            set(table1,'ColumnWidth',{60});
-            
-            table2 = uitable('Data',permute(Definition(:,ch,:),[1,3,2]),...
-                'ColumnName',num2cell(flist),...
-                'RowName',num2cell(tlim*1000));
-            set(table2,'ColumnWidth',{60});
-            
-    disptables(fig1,[table1 table2]);
-    end
-    if chans == 1
-        figure('Name', 'Clarity Index')
-    else
-        figure('Name', 'Clarity Index Average of All Channels')
-    end
-    
-    meanClarity = 10*log10(mean(10.^(ClarityIndex./10),2));
-    meanClarity = permute(meanClarity,[1,3,2]);
-    All = [meanClarity(1:index35-1,:); ...
-        meanClarity(index35+1:index50-1,:); ...
-        meanClarity(index50+1:index80-1,:); ...
-        meanClarity(index80+1:end,:)];
-    
-    semilogx(flist,All,'Color',[0.8 0.8 0.8])
-    hold on
-    semilogx(flist,meanClarity(index35,:),'-ro','LineWidth',1,'MarkerSize',8); 
-    semilogx(flist,meanClarity(index50,:),'-r+','LineWidth',1,'MarkerSize',8);
-    semilogx(flist,meanClarity(index80,:),'-rx','LineWidth',1,'MarkerSize',8);
-    grid on
-    xlabel('Band Frequency (Hz)');
-    ylabel('Clarity Index (dB)');
-    xlim([flist(1) flist(end)])
-    title('Clarity index, 10-100 ms time limits (o C35; + C50; x C80)')
-    %legend('show','Location','EastOutside');
-    hold off
-end
-
 end % eof
 
 %**************************************************************************
