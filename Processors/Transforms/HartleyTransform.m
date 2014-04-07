19ff09c22dbd7bbf96bd70d9c23d6bfabf52ef52
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-04-07 23:48:48 +0000

    cleanup of Hartley & Hilbert transforms

--- Processors/Transforms/HartleyTransform.m
+++ Processors/Transforms/HartleyTransform.m
@@ -1,6 +1,12 @@
 function out = HartleyTransform(in)
+% This function performs the Hartley transfrom of the input audio.
+% 
+% The Hartley transform is closely related to the Fourier transform,
+% but the result of transforming a real signal is entirely real. 
+% Furthermore, the Hartley transform is its own inverse transform.
 
 X = fft(in.audio);
+%X = fft(real(in.audio)); % alternatively only transform real data
 out.audio = (real(X) - imag(X)) ./ size(X,1).^0.5;
 out.funcallback.name = 'HartleyTransform.m';
 out.funcallback.inarg = {};
--- Processors/Transforms/HilbertTransform.m
+++ Processors/Transforms/HilbertTransform.m
@@ -1,12 +1,21 @@
-function out = Hilbert_Transform(in,method)
+function out = HilbertTransform(in,method)
 % A Hilbert transform is applied to the input audio. Note that the
-% resulting analytic waveform is complex. This can be dealt with in three
-% ways.
+% resulting analytic waveform is complex. This can be dealt with in various
+% ways:
+% 1. a complex wave is returned
+% 2. the real wave is returned to audio, and imaginary to audio2
+% 3. The absolute value (or envelope) of the complex wave is returned
+
+% other options... probably not useful (so not implemented yet)
+% 4. The instantaneous phase is returned
+% 5. The instantaneous frequency is returned (not audio)
+
+
 
 if nargin < 2
 method = menu('Output Format', ...
               'Complex wave', ...
-              'Real (audio) and Imag (audio1)',...
+              'Real (audio) and Imag (audio2)',...
               'Absolute value');
 end
 % Matlab's hilbert() seems to only work on up to 2-dimensional data,
