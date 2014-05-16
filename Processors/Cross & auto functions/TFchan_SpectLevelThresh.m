ab1a35b1483341cc437ee13ddf81c0f7a696bc02
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-05-16 08:12:26 +0000

    Update of TF (now works with additional audio input)

--- Processors/Cross & auto functions/TFchan_SpectLevelThresh.m
+++ Processors/Cross & auto functions/TFchan_SpectLevelThresh.m
@@ -7,7 +7,7 @@ function OUT = TFchan_SpectLevelThresh(in,fs,RefChan,Threshold,Duration,doplot)
 % inverse Fourier transform).
 %
 % Code by Densil Cabrera
-% version 1.0 (10 October 2013)
+% version 1.01 (16 May 2014)
 
 if nargin < 6, doplot = 0; end
 if nargin < 5, Duration = 2; end
@@ -15,39 +15,38 @@ if nargin < 4, Threshold = -60; end
 if nargin < 3
     RefChan = 1;
     if isstruct(in)
-        S = in.audio; % size of the IR
+        S = in.audio;
         fs = in.fs;
+        if size(S,3)>1
+            if isfield(in,'bandID')
+                bandID = in.bandID;
+            else
+                bandID = [];
+            end
+        end
     else
         S = in;
         if nargin < 2
             fs = inputdlg({'Sampling frequency [samples/s]'},...
-                               'Fs',1,{'48000'});
+                'Fs',1,{'48000'});
             fs = str2num(char(fs));
         end
+        bandID = [];
     end
-    ndim = ndims(S); % number of dimensions
-    switch ndim
-        case 2
-            len = length(S); % number of samples in IR
-            chans = size(S,2); % number of channels
-            bands = 1; % number of bands
-        case 3
-            len = length(S); % number of samples in IR
-            chans = size(S,2); % number of channels
-            bands = size(S,3); % number of bands
-    end
+    [len,chans,bands] = size(S);
+    
     
     maxDuration = floor((len) / in.fs);
     defaultDuration = 2;
     if defaultDuration > maxDuration, defaultDuration = maxDuration; end
     
     %dialog box for settings
-    prompt = {'Reference channel', 'Level threshold', 'Output duration', 'Plot (0 | 1)'};
+    prompt = {'Reference channel, OR 0 for additional audio input', 'Level threshold (dB)', 'Output duration (s) OR 0 for full duration', 'Plot (0 | 1)'};
     dlg_title = 'Settings';
     num_lines = 1;
     def = {'1','-60', num2str(defaultDuration),'0'};
     answer = inputdlg(prompt,dlg_title,num_lines,def);
-
+    
     if ~isempty(answer)
         RefChan = str2num(answer{1,1});
         Threshold = str2num(answer{2,1});
@@ -58,48 +57,102 @@ if nargin < 3
         return
     end
 end
-if isstruct(in)
-    S = in.audio; % size of the IR
-    fs = in.fs;
-else
-    S = in;
-    if nargin < 2
-        fs = inputdlg({'Sampling frequency [samples/s]'},...
-                           'Fs',1,{'48000'});
-        fs = str2num(char(fs));
+
+if RefChan == 0
+    selection = choose_audio; % call AARAE's choose_audio function
+    if ~isempty(selection)
+        refaudio = selection.audio; % additional audio data
+        fs2 = selection.fs; % sampling rate
+        
+        if ~(fs2 == fs)
+            % match sampling rates
+            refaudio = resample(refaudio,fs,fs2);
+            disp('Sampling rate mis-match - reference audio has been re-sampled.')
+        end
+        [len2, chans2, bands2] = size(refaudio); % new wave dimensions
+        fftlen = max([len len2]);
     end
-end
-ndim = ndims(S); % number of dimensions
-switch ndim
-    case 2
-        len = length(S); % number of samples in IR
-        chans = size(S,2); % number of channels
-        bands = 1; % number of bands
-    case 3
-        len = length(S); % number of samples in IR
-        chans = size(S,2); % number of channels
-        bands = size(S,3); % number of bands
+    if bands2 > 1
+        if bands2 ~= bands
+            refaudio = sum(refaudio,3);
+            disp('multiband reference audio has been mixed down')
+        else
+            if isfield(selection,'bandID') && ~isempty(bandID)
+                bandID2 = selection.bandID;
+                if bandID2 ~= bandID
+                    refaudio = sum(refaudio,3);
+                    disp('multiband reference audio has been mixed down')
+                end
+            end
+        end
+    end
+    if (chans2 > 1) %&& chans2 ~= chans
+        prompt = {'Reference channel'};
+        dlg_title = 'Reference audio settings';
+        num_lines = 1;
+        def = {'1'};
+        answer = inputdlg(prompt,dlg_title,num_lines,def);
+        
+        if ~isempty(answer)
+            RefChan2 = str2num(answer{1,1});
+            if RefChan2 > chans2
+                disp('Invalid reference channel entered - Channel 1 will be used instead')
+                RefChan2 = 1;
+            end
+            refaudio = refaudio(:,RefChan2,:);
+        else
+            OUT = [];
+            return
+        end
+    end
+else
+    if RefChan > chans
+        disp('Invalid reference channel entered - Channel 1 will be used instead')
+        RefChan = 1;
+    end
+    refaudio = S(:,RefChan,:);
+    fftlen = len;
 end
 
-if RefChan > chans, RefChan = 1; end
-outlen = floor(Duration * fs)+1;
-if outlen > len, outlen = len; end
 
-spectrum = fft(S);
+if Duration == 0
+    outlen = fftlen;
+else
+    outlen = floor(Duration * fs)+1;
+    if outlen > fftlen, outlen = fftlen; end
+end
+
+% if size(refaudio,3)>1
+%     disp('reference audio is multiband, so bands have been summed')
+%     refaudio = sum(refaudio,3);
+%     %bands2 = 1;
+% end
+
+spectrum = fft(S,fftlen);
+refspectrum = fft(refaudio,fftlen);
 
 magThreshold = 10.^(Threshold / 20);
 
 TF = zeros(len,chans,bands);
-for k = 1:bands
-    magnitude_ref = abs(spectrum(:,RefChan,k));
+if size(refspectrum,3) ==1
+    magnitude_ref = abs(refspectrum);
     maxmagnitude_ref = max(magnitude_ref);
     below_threshold = magnitude_ref < maxmagnitude_ref * magThreshold;
-    
-    Refspectrum = repmat(spectrum(:,RefChan), [1,chans]);
-    
-    TF(:,:,k) = conj(Refspectrum) .* spectrum(:,:,k) ./ (conj(Refspectrum) .* Refspectrum);
-    
-    TF(below_threshold,:,k) = 0; % zero all values below input wave threshold
+    refspectrumk = repmat(refspectrum, [1,chans,bands]);
+    TF = conj(refspectrumk) .* spectrum ./ (conj(refspectrumk) .* refspectrumk);
+    TF(below_threshold,:,:) = 0; % zero all values below input wave threshold
+else
+    for k = 1:bands
+        magnitude_ref = abs(refspectrum(:,1,k));
+        maxmagnitude_ref = max(magnitude_ref);
+        below_threshold = magnitude_ref < maxmagnitude_ref * magThreshold;
+        
+        refspectrumk = repmat(refspectrum(:,1,k), [1,chans,1]);
+        
+        TF(:,:,k) = conj(refspectrumk) .* spectrum(:,:,k) ./ (conj(refspectrumk) .* refspectrumk);
+        
+        TF(below_threshold,:,k) = 0; % zero all values below input wave threshold
+    end
 end
 
 % Return to time domain
@@ -109,6 +162,7 @@ out = ifft(TF);
 out = out(1:outlen,:,:);
 
 if isstruct(in)
+    OUT = in; % replicate input structure
     OUT.audio = out;
     OUT.funcallback.name = 'TFchan_SpectLevelThresh.m';
     OUT.funcallback.inarg = {fs,RefChan,Threshold,Duration,doplot};
@@ -116,6 +170,7 @@ else
     OUT = out;
 end
 
+% these plots are unnecessary within AARAE
 if doplot
     figure('Name', 'Transfer Function')
     
