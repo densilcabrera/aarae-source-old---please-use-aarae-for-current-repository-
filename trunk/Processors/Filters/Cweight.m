198ffaae5bedce8e3bf1667fb900f45d117281dd
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-05-30 11:24:32 +0000

    * sound logger analyser * bug fix to Bweight and Cweight

--- trunk/Processors/Filters/Cweight.m
+++ trunk/Processors/Filters/Cweight.m
@@ -8,9 +8,11 @@ if nargin < 2
     else
         audio = IN;
         fs = inputdlg({'Sampling frequency [samples/s]'},...
-                           'Fs',1,{'48000'});
+            'Fs',1,{'48000'});
         fs = str2num(char(fs));
     end
+else
+    audio = IN;
 end
     if ~isempty(audio) && ~isempty(fs)
         %if isdir([cd '/Processors/Filters/' num2str(fs) 'Hz'])
