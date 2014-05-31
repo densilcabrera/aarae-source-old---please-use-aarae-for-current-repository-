9106fc780dd0611295f482bb052cc86cab03a301
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-05-31 04:41:51 +0000

    update of sound logger - with output table & other changes

--- Analysers/Level and Strength/soundlogger.m
+++ Analysers/Level and Strength/soundlogger.m
@@ -28,18 +28,18 @@ if isstruct(IN)
     end
 end
 
-if nargin ==1 
-
-    param = inputdlg({'Window duration (s)';... 
-                      'Hop size between windows (s)';...
-                      'Integration time constant (s)';
-                      'Audio weighting [a|c|z]'},...
-                      'Sound Logger Settings',... 
-                      [1 60],... 
-                      {'15';'15';'0.125';'a'}); % default values
-
-    if length(param) < 4, param = []; end 
-    if ~isempty(param) 
+if nargin ==1
+    
+    param = inputdlg({'Window duration (s)';...
+        'Hop size between windows (s)';...
+        'Integration time constant (s)';
+        'Audio weighting [a|c|z]'},...
+        'Sound Logger Settings',...
+        [1 60],...
+        {'15';'15';'0.125';'a'}); % default values
+    
+    if length(param) < 4, param = []; end
+    if ~isempty(param)
         windowtime = str2num(char(param(1)));
         windowhop = str2num(char(param(2)));
         tau = str2num(char(param(3)));
@@ -48,19 +48,19 @@ if nargin ==1
 else
     param = [];
 end
-if isstruct(IN) 
-    audio = IN.audio; 
-    fs = IN.fs;       
+if isstruct(IN)
+    audio = IN.audio;
+    fs = IN.fs;
     
-   
+    
 elseif ~isempty(param) || nargin > 1
-                       
+    
     audio = IN;
-
+    
 end
 
 
-if ~isempty(audio) && ~isempty(fs)  
+if ~isempty(audio) && ~isempty(fs)
     [len, chans, bands] = size(audio);
     
     WindowLength = round(windowtime*fs);
@@ -86,9 +86,11 @@ if ~isempty(audio) && ~isempty(fs)
     % apply weighting
     if weighting == 'a' || weighting == 'A'
         
-            audio = Aweight(audio,fs);
+        audio = Aweight(audio,fs);
     elseif weighting == 'c' || weighting == 'C'
-            audio = Cweight(audio,fs);
+        audio = Cweight(audio,fs);
+    else
+        weighting = 'z'; % change to 'z' if it is not already 'z'
     end
     
     % apply temporal integration
@@ -125,7 +127,7 @@ if ~isempty(audio) && ~isempty(fs)
         Lmin(n,:,:) = min(L);
     end
     
-                    
+    
     if ~exist('bandID','var')
         bandID = 1:bands;
     end
@@ -168,7 +170,7 @@ if ~isempty(audio) && ~isempty(fs)
                 'Color',[0.7,0.7,1],...
                 'DisplayName','Lmin');
             plot(t,Leq(:,ch,b),...
-                'Color',[0,0,1],...
+                'Color',[1,0,0],...
                 'LineStyle',':', ...
                 'DisplayName','Leq');
             
@@ -186,36 +188,133 @@ if ~isempty(audio) && ~isempty(fs)
     end
     
     
-    for ch = 1:chans
-        if exist('chanID','var')
-            chanstring = char(chanID(ch));
-        else
-            chanstring = ['ch ',num2str(ch)];
+    figure('Name','Sound Logger Distributions ')
+    for b = 1:bands
+        subplot(r,c,b)
+        distributionPlot([reshape(Lmin(:,:,b),[nwin*chans,1]),...
+            reshape(L90(:,:,b),[nwin*chans,1]), ...
+            reshape(L50(:,:,b),[nwin*chans,1]),...
+            reshape(L10(:,:,b),[nwin*chans,1]),...
+            reshape(L5(:,:,b),[nwin*chans,1]),...
+            reshape(Lmax(:,:,b),[nwin*chans,1]), ...
+            reshape(Leq(:,:,b),[nwin*chans,1])]);
+        set(gca,'XTickLabel',{'Lmin';'L90';'L50';'L10';'L5';'Lmax';'Leq'})
+        ylim([ymin ymax])
+        xlabel('Parameter')
+        ylabel('Sound level (dB)')
+        if bands > 1
+            title([num2str(bandID(b)),' Hz'])
         end
-        figure('Name',['Sound Logger Distributions ', chanstring])
-        for b = 1:bands
-            subplot(r,c,b)
-            distributionPlot([Lmin(:,ch,b), L90(:,ch,b), ...
-                L50(:,ch,b), L10(:,ch,b), L5(:,ch,b), Lmax(:,ch,b), ...
-                Leq(:,ch,b)]);
-            set(gca,'XTickLabel',{'Lmin';'L90';'L50';'L10';'L5';'Lmax';'Leq'})
-            ylim([ymin ymax])
-            xlabel('Parameter')
-            ylabel('Sound level (dB)')
-            if bands > 1
-                title([num2str(bandID(b)),' Hz'])
-            end
-            %OUT.lines.(genvarname(['Soundloggerdist_ch' num2str(ch) '_' num2str(bandID(b)) 'Hz'])) = getplotdata;
-        end
+        % OUT.lines.(genvarname(['Soundloggerdist_'  num2str(bandID(b)) 'Hz'])) = getplotdata;
     end
     
-     
     
-%     fig1 = figure('Name','Sound Logger Summary');
-%     table1 = uitable('Data',[duration maximum minimum],...
-%                 'ColumnName',{'Duration','Maximum','Minimum'},...
-%                 'RowName',{'Results'});
-%     disptables(fig1,table1);
+    fig1 = figure('Name',['Sound Logger Summary ',weighting,'-weighted']);
+    data = [windowtime;windowhop;tau];
+    colname = {'Value'};
+    rowname = {'Duration of each window (s)';...
+        'Hop time from window to window (s)';...
+        'Integration time constant (s)'};
+    table1 = uitable('Data', data,...
+        'ColumnName', colname,...
+        'RowName', rowname);
+    
+    colname = {'Power mean';'Mean';'Median';'Minimum';'Maximum';'Range';...
+        '90th prctile';'10th prctile';'90-10% range'};
+    rowname = {'Leq';'Lmax';'L5';'L10';'L50';'L90';'Lmin'};
+    table2 = [];
+    
+    for b = 1:bands
+        if bands > 1
+            rowname = {['Leq ' num2str(bandID(b)) ' Hz'];...
+                ['Lmax ' num2str(bandID(b)) ' Hz'];...
+                ['L5 ' num2str(bandID(b)) ' Hz'];...
+                ['L10 ' num2str(bandID(b)) ' Hz'];...
+                ['L50 ' num2str(bandID(b)) ' Hz'];...
+                ['L90 ' num2str(bandID(b)) ' Hz'];...
+                ['Lmin ' num2str(bandID(b)) ' Hz']};
+        end
+        data = [10*log10(mean(reshape(10.^(Leq(:,:,b)./10),[nwin*chans,1]))),...
+            mean(reshape(Leq(:,:,b),[nwin*chans,1])),...
+            median(reshape(Leq(:,:,b),[nwin*chans,1])),...
+            min(reshape(Leq(:,:,b),[nwin*chans,1])),...
+            max(reshape(Leq(:,:,b),[nwin*chans,1])),...
+            max(reshape(Leq(:,:,b),[nwin*chans,1]))-min(reshape(Leq(:,:,b),[nwin*chans,1])),...
+            prctile(reshape(Leq(:,:,b),[nwin*chans,1]),90),...
+            prctile(reshape(Leq(:,:,b),[nwin*chans,1]),10),...
+            prctile(reshape(Leq(:,:,b),[nwin*chans,1]),90)-prctile(reshape(Leq(:,:,b),[nwin*chans,1]),10);...
+            ...
+            10*log10(mean(reshape(10.^(Lmax(:,:,b)./10),[nwin*chans,1]))),...
+            mean(reshape(Lmax(:,:,b),[nwin*chans,1])),...
+            median(reshape(Lmax(:,:,b),[nwin*chans,1])),...
+            min(reshape(Lmax(:,:,b),[nwin*chans,1])),...
+            max(reshape(Lmax(:,:,b),[nwin*chans,1])),...
+            max(reshape(Lmax(:,:,b),[nwin*chans,1]))-min(reshape(Lmax(:,:,b),[nwin*chans,1])),...
+            prctile(reshape(Lmax(:,:,b),[nwin*chans,1]),90),...
+            prctile(reshape(Lmax(:,:,b),[nwin*chans,1]),10),...
+            prctile(reshape(Lmax(:,:,b),[nwin*chans,1]),90)-prctile(reshape(Lmax(:,:,b),[nwin*chans,1]),10);...
+            ...
+            10*log10(mean(reshape(10.^(L5(:,:,b)./10),[nwin*chans,1]))),...
+            mean(reshape(L5(:,:,b),[nwin*chans,1])),...
+            median(reshape(L5(:,:,b),[nwin*chans,1])),...
+            min(reshape(L5(:,:,b),[nwin*chans,1])),...
+            max(reshape(L5(:,:,b),[nwin*chans,1])),...
+            max(reshape(L5(:,:,b),[nwin*chans,1]))-min(reshape(L5(:,:,b),[nwin*chans,1])),...
+            prctile(reshape(L5(:,:,b),[nwin*chans,1]),90),...
+            prctile(reshape(L5(:,:,b),[nwin*chans,1]),10),...
+            prctile(reshape(L5(:,:,b),[nwin*chans,1]),90)-prctile(reshape(L5(:,:,b),[nwin*chans,1]),10);...
+            ...
+            10*log10(mean(reshape(10.^(L10(:,:,b)./10),[nwin*chans,1]))),...
+            mean(reshape(L10(:,:,b),[nwin*chans,1])),...
+            median(reshape(L10(:,:,b),[nwin*chans,1])),...
+            min(reshape(L10(:,:,b),[nwin*chans,1])),...
+            max(reshape(L10(:,:,b),[nwin*chans,1])),...
+            max(reshape(L10(:,:,b),[nwin*chans,1]))-min(reshape(L10(:,:,b),[nwin*chans,1])),...
+            prctile(reshape(L10(:,:,b),[nwin*chans,1]),90),...
+            prctile(reshape(L10(:,:,b),[nwin*chans,1]),10),...
+            prctile(reshape(L10(:,:,b),[nwin*chans,1]),90)-prctile(reshape(L10(:,:,b),[nwin*chans,1]),10);...
+            ...
+            10*log10(mean(reshape(10.^(L50(:,:,b)./10),[nwin*chans,1]))),...
+            mean(reshape(L50(:,:,b),[nwin*chans,1])),...
+            median(reshape(L50(:,:,b),[nwin*chans,1])),...
+            min(reshape(L50(:,:,b),[nwin*chans,1])),...
+            max(reshape(L50(:,:,b),[nwin*chans,1])),...
+            max(reshape(L50(:,:,b),[nwin*chans,1]))-min(reshape(L50(:,:,b),[nwin*chans,1])),...
+            prctile(reshape(L50(:,:,b),[nwin*chans,1]),90),...
+            prctile(reshape(L50(:,:,b),[nwin*chans,1]),10),...
+            prctile(reshape(L50(:,:,b),[nwin*chans,1]),90)-prctile(reshape(L50(:,:,b),[nwin*chans,1]),10);...
+            ...
+            10*log10(mean(reshape(10.^(L90(:,:,b)./10),[nwin*chans,1]))),...
+            mean(reshape(L90(:,:,b),[nwin*chans,1])),...
+            median(reshape(L90(:,:,b),[nwin*chans,1])),...
+            min(reshape(L90(:,:,b),[nwin*chans,1])),...
+            max(reshape(L90(:,:,b),[nwin*chans,1])),...
+            max(reshape(L90(:,:,b),[nwin*chans,1]))-min(reshape(L90(:,:,b),[nwin*chans,1])),...
+            prctile(reshape(L90(:,:,b),[nwin*chans,1]),90),...
+            prctile(reshape(L90(:,:,b),[nwin*chans,1]),10),...
+            prctile(reshape(L90(:,:,b),[nwin*chans,1]),90)-prctile(reshape(L90(:,:,b),[nwin*chans,1]),10);...
+            ...
+            10*log10(mean(reshape(10.^(Lmin(:,:,b)./10),[nwin*chans,1]))),...
+            mean(reshape(Lmin(:,:,b),[nwin*chans,1])),...
+            median(reshape(Lmin(:,:,b),[nwin*chans,1])),...
+            min(reshape(Lmin(:,:,b),[nwin*chans,1])),...
+            max(reshape(Lmin(:,:,b),[nwin*chans,1])),...
+            max(reshape(Lmin(:,:,b),[nwin*chans,1]))-min(reshape(Lmin(:,:,b),[nwin*chans,1])),...
+            prctile(reshape(Lmin(:,:,b),[nwin*chans,1]),90),...
+            prctile(reshape(Lmin(:,:,b),[nwin*chans,1]),10),...
+            prctile(reshape(Lmin(:,:,b),[nwin*chans,1]),90)-prctile(reshape(Lmin(:,:,b),[nwin*chans,1]),10)];...
+            table2 = [table2 uitable('Data', data,...
+            'ColumnName', colname,...
+            'RowName', rowname)];
+    end
+    
+    [~,tables] = disptables(fig1,[table1 table2]);
+    OUT.tables = tables;
+    
+    %     table1 = uitable('Data',[duration maximum minimum],...
+    %                 'ColumnName',{'Duration','Maximum','Minimum'},...
+    %                 'RowName',{'Results'});
+    %     disptables(fig1,table1);
     % You may want to display your tables as barplots in teh AARAE
     % environment, in order to do this simply use the output of the
     % disptables funtion as follows:
@@ -224,9 +323,9 @@ if ~isempty(audio) && ~isempty(fs)
     %       OUT.tables = table;
     % If you have multiple tables to combine in the figure, you can
     % concatenate them:
-    % 
+    %
     %       disptables(fig1,[table1 table2 table3]);
-    % 
+    %
     % You may export these tables to be displayed as bar plots as if you
     % were doing it for a single table:
     %       [~,tables] = disptables(fig1,[table1 table2 table3]);
@@ -236,7 +335,7 @@ if ~isempty(audio) && ~isempty(fs)
     % field to display all your tables.
     
     % You may also include figures to display your results as plots.
-
+    
     % All figures created by your function are stored in the AARAE
     % environment under the results box. If your function outputs a
     % structure in OUT this saved under the 'Results' branch in AARAE and
@@ -250,34 +349,34 @@ if ~isempty(audio) && ~isempty(fs)
     % from your function. Remember to call the getplotdata function after
     % you have designed your chart. Currently this function only supports
     % barplots and lines. E.g.:
-    % 
+    %
     %       plot(t,audio)
     %       OUT.lines.thischart = getplotdata;
     
     % And once you have your result, you should set it up in an output form
     % that AARAE can understand.
-
-        %OUT = IN; % You can replicate the input structure for your output
-        %OUT.audio = audio; % And modify the fields you processed
-        % Or simply output the fields you consider necessary after
-        % processing the input audio data, AARAE will figure out what has
-        % changed and complete the structure. But remember, it HAS TO BE a
-        % structure if you're returning more than one field:
-        %   OUT = audio;  if you just want to return the audio,
-        %   or,
-        %   OUT.audio = audio; if you want to return two fields.
-        %   OUT.fs = fs;
-        OUT.funcallback.name = 'soundlogger.m'; % Provide AARAE
-        % with the name of your function 
-        OUT.funcallback.inarg = {windowtime, windowhop, tau, weighting, cal, fs}; % assign all of the 
-        % input parameters that could be used to call the function 
-        % without dialog box to the output field param (as a cell
-        % array) in order to allow batch analysing.
     
-% The processed audio data will be automatically displayed in AARAE's main
-% window as long as your output contains audio stored either as a single
-% variable: OUT = audio;, or it's stored in a structure along with any other
-% parameters: OUT.audio = audio;
+    %OUT = IN; % You can replicate the input structure for your output
+    %OUT.audio = audio; % And modify the fields you processed
+    % Or simply output the fields you consider necessary after
+    % processing the input audio data, AARAE will figure out what has
+    % changed and complete the structure. But remember, it HAS TO BE a
+    % structure if you're returning more than one field:
+    %   OUT = audio;  if you just want to return the audio,
+    %   or,
+    %   OUT.audio = audio; if you want to return two fields.
+    %   OUT.fs = fs;
+    OUT.funcallback.name = 'soundlogger.m'; % Provide AARAE
+    % with the name of your function
+    OUT.funcallback.inarg = {windowtime, windowhop, tau, weighting, cal, fs}; % assign all of the
+    % input parameters that could be used to call the function
+    % without dialog box to the output field param (as a cell
+    % array) in order to allow batch analysing.
+    
+    % The processed audio data will be automatically displayed in AARAE's main
+    % window as long as your output contains audio stored either as a single
+    % variable: OUT = audio;, or it's stored in a structure along with any other
+    % parameters: OUT.audio = audio;
 else
     % AARAE requires that in case that the user doesn't input enough
     % arguments to generate audio to output an empty variable.
@@ -288,28 +387,28 @@ end
 % Copyright (c) 2014, Densil Cabrera
 % All rights reserved.
 %
-% Redistribution and use in source and binary forms, with or without 
-% modification, are permitted provided that the following conditions are 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
 % met:
 %
 %  * Redistributions of source code must retain the above copyright notice,
 %    this list of conditions and the following disclaimer.
-%  * Redistributions in binary form must reproduce the above copyright 
-%    notice, this list of conditions and the following disclaimer in the 
+%  * Redistributions in binary form must reproduce the above copyright
+%    notice, this list of conditions and the following disclaimer in the
 %    documentation and/or other materials provided with the distribution.
 %  * Neither the name of the University of Sydney nor the names of its contributors
-%    may be used to endorse or promote products derived from this software 
+%    may be used to endorse or promote products derived from this software
 %    without specific prior written permission.
 %
-% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 % "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-% TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+% TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
 % PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
 % OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
 % EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
 % PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
 % PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-% LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+% LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 % NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 % SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 %**************************************************************************
