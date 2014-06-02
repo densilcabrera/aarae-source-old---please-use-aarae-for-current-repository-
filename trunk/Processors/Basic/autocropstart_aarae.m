2163881daa08fab4d31d5c2a1bb77ab084894fbb
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-06-02 06:02:35 +0000

    some more hints in analyser template, + autocrop_start now ok with non-structure input

--- trunk/Processors/Basic/autocropstart_aarae.m
+++ trunk/Processors/Basic/autocropstart_aarae.m
@@ -16,10 +16,11 @@ function out = autocropstart_aarae(in,threshold,method)
 % * a positive value is interpreted directly as a wave absolute amplitude 
 %
 % Code by Densil Cabrera
-% version 1.00 (25 March 2014)
+% version 1.01 (2 June 2014)
 
 if isstruct(in)
     out = in;
+    audio = in.audio;
     if nargin < 3
         prompt = {'IF NEGATIVE, Start truncation threshold below peak (in dB); OR IF POSITIVE, threshold amplitude of wave (not in dB)';...
             'Individual audio column truncation [1], Synchronous truncation using ensemble data [2], or Synchronous truncation using smallest truncation point from individual columns [3]'};
@@ -31,15 +32,15 @@ if isstruct(in)
         method = str2double(char(answer{2,1}));
     end
 else
-    %in.audio = in;
-    disp('autocropstart_aarae requires a structure as input 1')
-    out = [];
-    return
+    audio = in;
+%     disp('autocropstart_aarae requires a structure as input 1')
+%     out = [];
+%     return
 end
 
 
 if ~isempty(threshold) && ~isempty(method)
-    [len,chans,bands,dim4,dim5,dim6] = size(in.audio);
+    [len,chans,bands,dim4,dim5,dim6] = size(audio);
     switch method
         case 1
             % individual start truncation (with loss of synchrony)
@@ -49,19 +50,19 @@ if ~isempty(threshold) && ~isempty(method)
                         for d5 = 1:dim5
                             for d6 = 1:dim6
                                 if threshold <= 0
-                                    threshind = find(abs(in.audio(:,ch,b,d4,d5,d6))...
-                                        >= max(abs(in.audio(:,ch,b,d4,d5,d6))) ...
+                                    threshind = find(abs(audio(:,ch,b,d4,d5,d6))...
+                                        >= max(abs(audio(:,ch,b,d4,d5,d6))) ...
                                         .* 10.^(threshold/20),...
                                         1,'first');
                                 else
-                                    threshind = find(abs(in.audio(:,ch,b,d4,d5,d6))...
+                                    threshind = find(abs(audio(:,ch,b,d4,d5,d6))...
                                         >= threshold,1,'first');
                                 end
                                 if ~isempty(threshind)
                                     if threshind > 1 && threshind < len
-                                        in.audio(1:threshind-1,ch,b,d4,d5,d6) = 0;
-                                        in.audio(:,ch,b,d4,d5,d6) = ...
-                                            circshift(in.audio(:,ch,b,d4,d5,d6)...
+                                        audio(1:threshind-1,ch,b,d4,d5,d6) = 0;
+                                        audio(:,ch,b,d4,d5,d6) = ...
+                                            circshift(audio(:,ch,b,d4,d5,d6)...
                                             ,-(threshind-1));
                                     end
                                 end
@@ -70,10 +71,14 @@ if ~isempty(threshold) && ~isempty(method)
                     end
                 end
             end
-            out.audio = in.audio;
+            if isstruct(in)
+                out.audio = audio;
+            else
+                out = audio;
+            end
         case 2
             % use mixed signal to find truncation point
-            mixed = mean(mean(mean(mean(mean(in.audio,6),5),4),3),2);
+            mixed = mean(mean(mean(mean(mean(audio,6),5),4),3),2);
             if threshold <= 0
                 threshind = find(abs(mixed) >= max(abs(mixed))...
                     .* 10.^(threshold/20),1,'first');
@@ -82,7 +87,11 @@ if ~isempty(threshold) && ~isempty(method)
             end
             if ~isempty(threshind)
                 if threshind > 1 && threshind <len
-                    out.audio = in.audio(threshind:end,:,:,:,:,:);
+                    if isstruct(in)
+                        out.audio = audio(threshind:end,:,:,:,:,:);
+                    else
+                        out = audio(threshind:end,:,:,:,:,:);
+                    end
                 end
             end
             
@@ -96,12 +105,12 @@ if ~isempty(threshold) && ~isempty(method)
                         for d5 = 1:dim5
                             for d6 = 1:dim6
                                 if threshold <= 0
-                                    threshindnew = find(abs(in.audio(:,ch,b,d4,d5,d6))...
-                                        >= max(abs(in.audio(:,ch,b,d4,d5,d6))) ...
+                                    threshindnew = find(abs(audio(:,ch,b,d4,d5,d6))...
+                                        >= max(abs(audio(:,ch,b,d4,d5,d6))) ...
                                         .* 10.^(threshold/20),...
                                         1,'first');
                                 else
-                                    threshindnew = find(abs(in.audio(:,ch,b,d4,d5,d6))...
+                                    threshindnew = find(abs(audio(:,ch,b,d4,d5,d6))...
                                         >= threshold,1,'first');
                                 end
                                 if ~isempty(threshindnew)
@@ -117,14 +126,19 @@ if ~isempty(threshold) && ~isempty(method)
             end
             
             if threshind > 1 && threshind <=len
-                out.audio = in.audio(threshind:end,:,:,:,:,:);
+                if isstruct(in)
+                    out.audio = audio(threshind:end,:,:,:,:,:);
+                else
+                    out = audio(threshind:end,:,:,:,:,:);
+                end
             end
             
     end
     
-    
-    out.funcallback.name = 'autocropstart_aarae.m';
-    out.funcallback.inarg = {threshold,method};
+    if isstruct(in)
+        out.funcallback.name = 'autocropstart_aarae.m';
+        out.funcallback.inarg = {threshold,method};
+    end
 else
     out = [];
 end
