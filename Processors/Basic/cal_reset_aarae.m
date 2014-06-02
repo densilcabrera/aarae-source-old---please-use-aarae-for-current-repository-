dd903c448eba9f81c01d8ea0b31825d063300bcf
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-06-02 00:07:29 +0000

    cal reset now also accepts non-structure input

--- Processors/Basic/cal_reset_aarae.m
+++ Processors/Basic/cal_reset_aarae.m
@@ -1,38 +1,57 @@
-function out = cal_reset_aarae(in,outcal)
+function out = cal_reset_aarae(in,outcal,incal)
 % This function resets the .cal field of an AARAE audio object to a
 % user-specified value. Gain is applied to the underlying audio waveform to
 % compensate for the .cal field change. For multichannel audio, this
 % function will reset the .cal to be the same for all channels.
 %
+% This function can also be used to interpret the cal value of audio so as
+% to apply gain (hence it could be called by another AARAE function to
+% simplify the code used to interpret the cal value). The third input
+% argument (incal) is used to specify the original cal value for cases
+% where the input is not in the AARAE structure format (it is not used if 
+% the input is an AARAE structure).
+%
 % Code by Densil Cabrera
-% version 1.00 (23 May 2014)
+% version 1.01 (2 June 2014)
 
 if isstruct(in)
     %audio = in.audio;
-   if isfield(in,'cal')
-       cal = in.cal;
-       [len,chans,bands,dim4,dim5,dim6] = size(in.audio);
-       if length(cal) ~= chans
-           if length(cal) == 1
-               cal = repmat(cal,[1,chans]);
-           else
-               warndlg('Cal size does not match audio channels','AARAE info','modal')
-               out = [];
-               return
-           end
-       end
-   else
-       h = warndlg('Cal field does not exist','AARAE info','modal');
-       uiwait(h)
-       out = [];
-       return
-   end
-
+    if isfield(in,'cal')
+        cal = in.cal;
+        [len,chans,bands,dim4,dim5,dim6] = size(in.audio);
+        if length(cal) ~= chans
+            if length(cal) == 1
+                cal = repmat(cal,[1,chans]);
+            else
+                warndlg('Cal size does not match audio channels','AARAE info','modal');
+                out = [];
+                return
+            end
+        end
+    else
+        h = warndlg('Cal field does not exist','AARAE info','modal');
+        uiwait(h)
+        out = [];
+        return
+    end
+    
 else
-    h = warndlg('Input audio must be in an AARAE structure','AARAE info','modal');
-    uiwait(h)
-    out = [];
-    return
+    [len,chans,bands,dim4,dim5,dim6] = size(in);
+    if ~exist ('incal','var')
+        cal = zeros(1,chans); % default 0 dB cal for input
+    else
+        cal = incal;
+        if length(incal) ~= chans
+            if length(incal) == 1
+                cal = repmat(incal,[1,chans]);
+            else
+                h=warndlg('incal size does not match audio channels','AARAE info','modal');
+                uiwait(h)
+                out = [];
+                return
+            end
+        end
+    end
 end
 
 
@@ -54,13 +73,18 @@ end
 
 
 if ~isempty(outcal)
-    out = in;
-    gain = 10.^((cal - outcal)./20);
-    out.cal = repmat(outcal,[1,chans]);
-    out.audio = out.audio .* repmat(gain,[len,1,bands,dim4,dim5,dim6]);
-    % to do: consider how to operate on audio2
-    out.funcallback.name = 'cal_reset_aarae.m';
-    out.funcallback.inarg = {outcal};
+    if isstruct(in)
+        out = in;
+        gain = 10.^((cal - outcal)./20);
+        out.cal = repmat(outcal,[1,chans]);
+        out.audio = out.audio .* repmat(gain,[len,1,bands,dim4,dim5,dim6]);
+        % to do: consider how to operate on audio2
+        out.funcallback.name = 'cal_reset_aarae.m';
+        out.funcallback.inarg = {outcal};
+    else
+        gain = 10.^((cal - outcal)./20);
+        out = in .* repmat(gain,[len,1,bands,dim4,dim5,dim6]);
+    end
 else
     out = [];
 end
