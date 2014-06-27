91c5596547c1397289923bc1f7ec5937813f269b
danieljimo@gmail.com <danieljimo@gmail.com@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-06-27 01:55:22 +0000

    * Addition of custom colormap for 3D result charts. * YAxisLabel fixed for imagesc charts for result leafs * Colormap responsive to number of lines plotted in charts with HSV colors

--- Framework/doresultplot.m
+++ Framework/doresultplot.m
@@ -12,32 +12,34 @@ try
     catorcont = tabledata(:,4);
     if any(cellfun(@isempty,catorcont)), catorcont(cellfun(@isempty,catorcont)) = {false}; end
     naxis = length(find([catorcont{:}] == true));
+    eval(['data = squeeze(audiodata.data(' sel '));'])
     if naxis < 2
+        cmap = colormap(hsv(size(data,2)));
+        set(handles.aarae,'DefaultAxesColorOrder',cmap)
         if ~strcmp(chartfunc,'distributionPlot') && ~strcmp(chartfunc,'boxplot')
+            cla(haxes,'reset')
             axis = find([catorcont{:}] == true);
             Xdata = audiodata.(genvarname(tabledata{axis(1,1),1}));
             if ~isnumeric(Xdata)
                 if iscell(Xdata), Xdata = cell2mat(Xdata); end
             end
             if ~isequal(size(Xdata),size(audiodata.data))
-                eval([chartfunc '(haxes,Xdata,squeeze(audiodata.data(' sel ')))'])
+                eval([chartfunc '(haxes,Xdata,data)'])
             else
-                eval([chartfunc '(haxes,squeeze(Xdata(' sel ')),squeeze(audiodata.data(' sel ')))'])
+                eval([chartfunc '(haxes,squeeze(Xdata(' sel ')),data)'])
             end
             xlabel(haxes,strrep([tabledata{axis(1,1),1} ' [' audiodata.(genvarname([tabledata{axis(1,1),1} 'info'])).units ']'],'_',' '))
             ylabel(haxes,strrep(audiodata.datainfo.units,'_',' '))
         else
             cla(haxes,'reset')
-            singdim = [];
             catdim = find(cellfun(@isempty,tabledata(:,4)),1);
             if isempty(catdim), catdim = 1; end
             eval(['catdim = length(size(audiodata.data(' sel ')));'])
-            if isempty(singdim), singdim = 1; end
             if strcmp(chartfunc,'distributionPlot')
-                eval([chartfunc '(haxes,squeeze(audiodata.data(' sel ')),''xNames'',audiodata.(genvarname(cattable.Data{catdim,1}))(' cattable.Data{catdim,2} '))'])
+                eval([chartfunc '(haxes,data,''xNames'',audiodata.(genvarname(cattable.Data{catdim,1}))(' cattable.Data{catdim,2} '))'])
             end
             if strcmp(chartfunc,'boxplot')
-                eval([chartfunc '(haxes,squeeze(audiodata.data(' sel ')),''labels'',audiodata.(genvarname(cattable.Data{catdim,1}))(' cattable.Data{catdim,2} '))'])
+                eval([chartfunc '(haxes,data,''labels'',audiodata.(genvarname(cattable.Data{catdim,1}))(' cattable.Data{catdim,2} '))'])
             end
             xlabel(haxes,strrep([cattable.Data{catdim,1} ' [' audiodata.(genvarname([cattable.Data{catdim,1} 'info'])).units ']'],'_',' '))
             ylabel(haxes,strrep(audiodata.datainfo.units,'_',' '))
@@ -45,6 +47,7 @@ try
         handles.tabledata = tabledata;
         guidata(handles.aarae,handles)
     elseif naxis == 2
+        cla(haxes,'reset')
         axis = find([catorcont{:}] == true);
         Xdata = audiodata.(genvarname(tabledata{axis(1,1),1})); 
         Ydata = audiodata.(genvarname(tabledata{axis(1,2),1}));
@@ -54,18 +57,22 @@ try
         if ~isnumeric(Ydata)
             if iscell(Ydata), Ydata = cell2mat(Ydata); end
         end
+        aaraecmap = importdata([cd '/Utilities/aaraecmap.mat']);
         if ~strcmp(chartfunc,'imagesc')
-            eval(['data = squeeze(audiodata.data(' sel '));'])
             if ~isequal([length(Ydata),length(Xdata)],size(data)), data = data'; end %#ok : Used in line above
             eval([chartfunc '(haxes,Xdata,Ydata,data)'])
+            colormap(aaraecmap)
+            %cmap = colormap(cool(size(data,1)));
+            %set(haxes,'ColorOrder',cmap)
         else
-            eval([chartfunc '(Xdata,Ydata,squeeze(audiodata.data(' sel ')),''Parent'',haxes)'])
+            eval([chartfunc '(Xdata,1:length(Ydata),data,''Parent'',haxes)'])
+            set(haxes,'YTickLabel',num2str(Ydata'))
             set(haxes,'YDir','normal')
+            colormap(aaraecmap)
         end
         xlabel(haxes,strrep([tabledata{axis(1,1),1} ' [' audiodata.(genvarname([tabledata{axis(1,1),1} 'info'])).units ']'],'_',' '))
         ylabel(haxes,strrep([tabledata{axis(1,2),1} ' [' audiodata.(genvarname([tabledata{axis(1,2),1} 'info'])).units ']'],'_',' '))
         zlabel(haxes,strrep(audiodata.datainfo.units,'_',' '))
-        colormap('Jet')
         handles.tabledata = tabledata;
         guidata(handles.aarae,handles)
     else
