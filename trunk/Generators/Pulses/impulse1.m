ca8d0c082b9022bc66b8d401710d1954b1142daf
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-08-01 04:44:48 +0000

    Method for dealing with pulse trains added (by modifying the impulse1 generator)

--- trunk/Generators/Pulses/impulse1.m
+++ trunk/Generators/Pulses/impulse1.m
@@ -1,6 +1,26 @@
 function OUT = impulse1(duration_pre,duration_post,amplitude, fs)
-% Generates a Kronecker delta function with a specified duration of silence
-% before and/or after.
+% This function generates a Kronecker delta function with a specified
+% duration of silence before and/or after. This provides a signal that can
+% be used for the direct measurement of impulse response, although it is
+% highly susceptible to contamination from noise.
+%
+% Using the AARAE generator dialog box, before running this function, you
+% can generate a pulse train, which can be useful for:
+%  * Synchronous averaging (to remove background noise contamination); or
+%  * Examining time variance in low noise systems.
+%
+% As well as the audio wave, this function outputs its inverse filter
+% (which is trivial - i.e. a simple time reversal of the delta function) so
+% that AARAE's convolve audio with audio2 (*) button can be used to perform
+% synchronous averaging or IR stacking (for time variance analysis) from a
+% recording of a pulse train. Problems may occur if the gap between
+% impulses when generated in the pulse train is too short for the system's
+% latency (however a gap of 1 s should usually be safe, and delay
+% compensation in AARAE's Syscal may help).
+%
+% Code by Densil Cabrera
+% version 2 (1 August 2014)
+
 if nargin == 0
     param = inputdlg({'Duration of silence before impulse [s]';...
         'Duration of silence after impulse [s]';...
@@ -38,6 +58,7 @@ if ~isempty(param) || nargin ~= 0
     tag = ['Impulse_' num2str(samples_pre) '_' num2str(samples_post) '_' num2str(amplitude)];
     
     OUT.audio = y;
+    OUT.audio2 = flipud(y);
     OUT.fs = fs;
     OUT.tag = tag;
     OUT.funcallback.name = 'impulse1.m';
