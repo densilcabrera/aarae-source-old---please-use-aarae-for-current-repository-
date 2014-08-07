4b97b67f2cfee1adefae11f09f2a8331871c9132
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-08-07 03:57:13 +0000

    non-lin distortion update

--- Generators/Tones/stepped_tone_log.m
+++ Generators/Tones/stepped_tone_log.m
@@ -1,10 +1,11 @@
 function OUT = stepped_tone_log(duration, lof, hif, fstep, fs)
 % Generates a sequence of pure tones (with frequencies exponentially
 % spaced), separated by silence. By default each tone has a duration of 1
-% s. The silence duration is half that of the tone. This signal may be
-% useful in the direct measurement of harmonic distortion, or indeed in the
-% measurement of linear transfer function (although there are much more
-% efficient ways of doing that!).
+% s (or a little less, because the tone is present for an integer number of
+% cycles to avoid clicks). The silence duration is half that of the nominal
+% tone duration. This signal may be useful in the direct measurement of
+% harmonic distortion, or indeed in the measurement of linear transfer
+% function (although there are much more efficient ways of doing that!).
 %
 % The frequencies used are adjusted so that they have an integer number of
 % samples in their exact period - which facilitates analsysis (e.g., using
@@ -13,16 +14,18 @@ function OUT = stepped_tone_log(duration, lof, hif, fstep, fs)
 % adjustment unless a high sampling rate is used (which is advisable in
 % harmonic distortion analysis anyway).
 %
+% The highest frequency allowed is fs/4, and the lowest is 10 Hz.
+%
 % The tone sequence is replicated in audio2. Additional properties fields
 % are generated to be used by an analyser.
 %
 %
 % Code by Densil Cabrera
-% Version 0 (3 August 2014)
+% Version 1.00 (7 August 2014)
 
 
 if nargin == 0
-    param = inputdlg({'Duration of each tone (integer, minimum 1 s) [s]';...
+    param = inputdlg({'Approximate duration of each tone (minimum 1 s) [s]';...
                        'Lowest tone frequency (integer) [Hz]';...
                        'Highest tone frequency (integer) [Hz]';...
                        'Fractional octave step (e.g. 1 | 3 | 12)';...
@@ -32,7 +35,7 @@ if nargin == 0
     param = str2num(char(param));
     if length(param) < 5, param = []; end
     if ~isempty(param)
-        duration = round(param(1));
+        duration = param(1);
         lof = param(2);
         hif = param(3);
         fstep = param(4);
@@ -43,6 +46,7 @@ if nargin == 0
     end
 end
 if ~isempty(param) || nargin ~= 0
+    if duration < 1, duration = 1; end
     if lof < 10, lof = 10; end
     if hif > fs/4, hif = fs/4; end
     flist = lof * 2.^(0:1/fstep:ceil(log2(hif/lof)));
@@ -51,9 +55,11 @@ if ~isempty(param) || nargin ~= 0
     t = linspace(0,duration,fs*duration);
     y = zeros(length(flist) * 1.5 * duration * fs, 1);
     startindex = 1+((1:length(flist))-1)*1.5*duration*fs;
-    endindex = startindex + duration*fs -1;
+    % make sure only an integer number of cyles is generated
+    tonelen = floor(fs*duration./(fs./flist)) * fs./flist;
+    endindex = startindex + tonelen-1;
     for n = 1:length(flist)
-        y(startindex(n):endindex(n)) = sin(2*pi*flist(n).*t');
+        y(startindex(n):endindex(n)) = sin(2*pi*flist(n).*t(1:tonelen(n))');
     end
     
     tag = 'SteppedToneSeqence';
