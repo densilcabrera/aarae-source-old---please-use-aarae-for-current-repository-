0cecfef0d2b64015b8f167528e79f5f15a7bc272
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-08-19 00:22:38 +0000

    New bandpass filter processor, and added bandpass filtering to beamplotting

--- Analysers/Spatial Analysis/beamplotting.m
+++ Analysers/Spatial Analysis/beamplotting.m
@@ -1,4 +1,18 @@
-function OUT = beamplotting(IN,fs,sphere_cover,start_time,end_time,max_order)
+function OUT = beamplotting(IN,fs,sphere_cover,start_time,end_time,max_order,hif,lof)
+% This function creates a plot of spatial energy distribution from spherical
+% harmonic encoded (HOA) multichannel audio input data.
+%
+% If audio is multiband, then a separate plot is created for each band.
+%
+% Diffusivity of the soundfield is calculated using the Gover and HOA
+% covariance methods.
+%
+% This function uses the HOAToolbox, by Nicholas Epain.
+%
+% Code by Daniel Jimenez, Luis Miranda and Densil Cabrera
+% Version 1.00 (19 August 2014)
+
+
 
 if isstruct(IN)
     hoaSignals = IN.audio;
@@ -21,13 +35,17 @@ if isstruct(IN)
         param = inputdlg({'Number of points on the sphere',...
                           'Start time [s]'...
                           'End time [s]'...
-                          'Maximum order'},...
+                          'Maximum order'...
+                          'High frequency cutoff (Hz)'...
+                          'Low frequency cutoff (Hz)'},...
                          'Input parameters',1,...
                          {num2str(sphere_cover),...
                           num2str(start_time),...
                           num2str(end_time),...
-                          num2str(max_order)});
-        if isempty(param) || isempty(param{1,1}) || isempty(param{2,1}) || isempty(param{3,1}) || isempty(param{4,1})
+                          num2str(max_order),...
+                          num2str(fs/2),...
+                          '0'});
+        if isempty(param) || isempty(param{1,1}) || isempty(param{2,1}) || isempty(param{3,1}) || isempty(param{4,1}) || isempty(param{5,1}) || isempty(param{6,1})
             OUT = [];
             return;
         else
@@ -41,10 +59,19 @@ if isstruct(IN)
             end_sample = round(end_time*fs);
             if end_sample >= length(hoaSignals), end_sample = length(hoaSignals); end
             max_order = str2double(param{4,1});
-            if isnan(sphere_cover) || isnan(start_time) || isnan(end_time) || isnan(max_order), OUT = []; return; end
+            hif = str2double(param{5,1});
+            lof = str2double(param{6,1});
+            if isnan(sphere_cover) || isnan(start_time) || isnan(end_time) || isnan(max_order) || isnan(hif) || isnan(lof), OUT = []; return; end
         end
     end
 end
+
+if (hif < fs/2 && hif > lof) || (lof > 0 && lof < hif)
+    % bandlimit the spectrum
+    filterorder = 48;
+    hoaSignals = bandpass(hoaSignals, lof, hif, filterorder, fs);
+end
+
 
 [azim_for_directplot,elev_for_directplot] = SphereCovering(sphere_cover);
 
@@ -104,7 +131,37 @@ end
 
 if isstruct(IN)
     OUT.funcallback.name = 'beamplotting.m';
-    OUT.funcallback.inarg = {fs,sphere_cover,start_time,end_time,max_order};
+    OUT.funcallback.inarg = {fs,sphere_cover,start_time,end_time,max_order,hif,lof};
 else
     OUT = hoaSignals;
 end
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright (c) 2014, Daniel Jimenez, Luis Miranda and Densil Cabrera
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
+% met:
+%
+%  * Redistributions of source code must retain the above copyright notice,
+%    this list of conditions and the following disclaimer.
+%  * Redistributions in binary form must reproduce the above copyright
+%    notice, this list of conditions and the following disclaimer in the
+%    documentation and/or other materials provided with the distribution.
+%  * Neither the name of the University of Sydney nor the names of its contributors
+%    may be used to endorse or promote products derived from this software
+%    without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+% TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+% PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+% OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+% EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+% PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+% PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+% LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
