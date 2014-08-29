9654e5daf7ecf0c5ea1e60c1f099000a8065719d
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-08-29 12:05:56 +0000

    new beamplotting plot types (surf and surfl)

--- trunk/Analysers/Spatial Analysis/beamplotting.m
+++ trunk/Analysers/Spatial Analysis/beamplotting.m
@@ -1,4 +1,4 @@
-function OUT = beamplotting(IN,fs,sphere_cover,start_time,end_time,max_order,hif,lof)
+function OUT = beamplotting(IN,fs,sphere_cover,start_time,end_time,max_order,hif,lof,plottype)
 % This function creates a plot of spatial energy distribution from spherical
 % harmonic encoded (HOA) multichannel audio input data.
 %
@@ -20,12 +20,15 @@ if isstruct(IN)
 else
     if nargin < 2
         fs = inputdlg({'Sampling frequency [samples/s]'},...
-                           'Fs',1,{'48000'});
+            'Fs',1,{'48000'});
         fs = str2double(char(fs));
     end
     hoaSignals = IN;
 end
 
+if nargin < 9, plottype = 1; end
+if nargin < 8, lof = 0; end
+if nargin < 7, hif = fs/2; end
 if nargin < 6, max_order=round(size(hoaSignals,2).^0.5-1); end
 if nargin < 5, end_time = length(hoaSignals)/fs; end
 if nargin < 4, start_time = 0; end
@@ -33,25 +36,27 @@ if nargin < 3, sphere_cover = 130; end
 if isstruct(IN)
     if nargin < 3
         param = inputdlg({'Number of points on the sphere',...
-                          'Start time [s]'...
-                          'End time [s]'...
-                          'Maximum order'...
-                          'High frequency cutoff (Hz)'...
-                          'Low frequency cutoff (Hz)'},...
-                         'Input parameters',1,...
-                         {num2str(sphere_cover),...
-                          num2str(start_time),...
-                          num2str(end_time),...
-                          num2str(max_order),...
-                          num2str(fs/2),...
-                          '0'});
+            'Start time [s]'...
+            'End time [s]'...
+            'Maximum order'...
+            'High frequency cutoff (Hz)'...
+            'Low frequency cutoff (Hz)'...
+            'PLOT TYPE: Robinson Projection in dB [1]; 3D colormap surface in amplitude [2]; 3D lit surface in amplitude (slow) [3]'},...
+            'Input parameters',1,...
+            {num2str(sphere_cover),...
+            num2str(start_time),...
+            num2str(end_time),...
+            num2str(max_order),...
+            num2str(fs/2),...
+            '0',...
+            '1'});
         if isempty(param) || isempty(param{1,1}) || isempty(param{2,1}) || isempty(param{3,1}) || isempty(param{4,1}) || isempty(param{5,1}) || isempty(param{6,1})
             OUT = [];
             return;
         else
             sphere_cover = str2double(param{1,1});
             if sphere_cover>130,sphere_cover = 130;end
-            if sphere_cover<4,sphere_cover = 4;end    
+            if sphere_cover<4,sphere_cover = 4;end
             start_time = str2double(param{2,1});
             start_sample = round(start_time*fs);
             if start_sample == 0, start_sample = 1; end
@@ -70,6 +75,7 @@ if isstruct(IN)
             end
             hif = str2double(param{5,1});
             lof = str2double(param{6,1});
+            plottype = str2double(param{7,1});
             if isnan(sphere_cover) || isnan(start_time) || isnan(end_time) || isnan(max_order) || isnan(hif) || isnan(lof), OUT = []; return; end
         end
     end
@@ -81,10 +87,31 @@ if (hif < fs/2 && hif > lof) || (lof > 0 && lof < hif)
     hoaSignals = bandpass(hoaSignals, lof, hif, filterorder, fs);
 end
 
+switch plottype
+    case {2, 3}
+        % rectangular mesh
+        if plottype == 2
+            step = pi/(6 * max_order);
+        elseif plottype == 3
+            step = pi/(12 * max_order);
+        end
+        elev_for_directplot = -pi/2:step:pi/2; % elevation
+        azim_for_directplot = 0:2*step:2*pi; % azimuth
+        [azim_for_directplot, elev_for_directplot] =...
+            meshgrid(azim_for_directplot, elev_for_directplot);
+        [len1, len2] = size(azim_for_directplot);
+        
+        azim_for_directplot = reshape(azim_for_directplot,numel(azim_for_directplot),1);
+        elev_for_directplot = reshape(elev_for_directplot,numel(elev_for_directplot),1);
+        numberofdirections = numel(azim_for_directplot);
+    otherwise
+        % case 1
+        % approximately even sphere covering
+        [azim_for_directplot,elev_for_directplot] = SphereCovering(sphere_cover);
+        numberofdirections = sphere_cover;
+end
 
-[azim_for_directplot,elev_for_directplot] = SphereCovering(sphere_cover);
-
-beams_for_directplot = GenerateSpkFmt('sphCoord',[azim_for_directplot elev_for_directplot ones(sphere_cover,1)]);
+beams_for_directplot = GenerateSpkFmt('sphCoord',[azim_for_directplot elev_for_directplot ones(numberofdirections,1)]);
 
 hoa2SpkOpt_for_directplot = GenerateHoa2SpkOpt('decodType','basic','sampFreq',fs,'filterLength',128,'transFreq',6000,...
     'transWidth',200,'spkDistCor',false);
@@ -101,20 +128,42 @@ direct_sound_HOA = hoaSignals(start_sample:end_sample,:,:);
 
 bands = size(hoaSignals,3);
 
-beamsignals_for_directPlot = zeros(length(direct_sound_HOA),sphere_cover,bands);
+beamsignals_for_directPlot = zeros(length(direct_sound_HOA),numberofdirections,bands);
 
 for i = 1:size(hoaSignals,2);
-    for j = 1:sphere_cover;
+    for j = 1:numberofdirections;
         for b = 1:bands
             beamsignals_for_directPlot(:,j,b) = beamsignals_for_directPlot(:,j,b)+(direct_sound_HOA(:,i,b).*hoa2SpkCfg_for_directplot.filters.gainMatrix(j,i));
         end
     end
 end
 
+
 [Goverdif,HOAdif] = deal(zeros(bands,1));
 for b = 1:bands
-    PlotRobinsonProject([azim_for_directplot,elev_for_directplot],mag2db(sum(abs(beamsignals_for_directPlot(:,:,b)))'));
-    
+    switch plottype
+        case {2, 3}
+            figure
+            azim_for_directplot = reshape(azim_for_directplot,len1,len2);
+            elev_for_directplot = reshape(elev_for_directplot,len1,len2);
+            values = reshape(sum(abs(beamsignals_for_directPlot(:,:,b))),len1,len2);
+            [x,y,z] = sph2cart(azim_for_directplot,elev_for_directplot,values);
+            if plottype == 2
+                surf(x,y,z,values,'FaceColor','interp','FaceLighting','phong');
+                camlight right
+            elseif plottype ==3
+                surfl(x,y,z,'light');
+                shading interp
+                colormap(copper)
+            end
+            xlabel('x')
+            ylabel('y')
+            zlabel('z')
+            set(gca, 'DataAspectRatio', [1 1 1])
+            set(gca, 'PlotBoxAspectRatio', [1 1 1])
+        otherwise
+            PlotRobinsonProject([azim_for_directplot,elev_for_directplot],mag2db(sum(abs(beamsignals_for_directPlot(:,:,b)))'));
+    end
     % Diffuseness calculations and display
     Goverdif(b) = GoverDiffuseness(direct_sound_HOA(:,:,b),hoaFmt);
     HOAdif(b) = HoaDiffuseness(direct_sound_HOA(:,:,b),hoaFmt);
@@ -130,7 +179,7 @@ for b = 1:bands
         end
     else
         title(['Gover dif. ',...
-                num2str(Goverdif(b)),', HOA dif. ', num2str(HOAdif(b))])
+            num2str(Goverdif(b)),', HOA dif. ', num2str(HOAdif(b))])
     end
     
     
@@ -140,7 +189,7 @@ end
 
 if isstruct(IN)
     OUT.funcallback.name = 'beamplotting.m';
-    OUT.funcallback.inarg = {fs,sphere_cover,start_time,end_time,max_order,hif,lof};
+    OUT.funcallback.inarg = {fs,sphere_cover,start_time,end_time,max_order,hif,lof,plottype};
 else
     OUT = hoaSignals;
 end
