060aeaa585b41b5a68296102586504295ef828f7
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-09-06 12:21:49 +0000

    Stage support: added preliminary charts; changed filters; need to test properly

--- Analysers/Energy ratios/StageSupport.m
+++ Analysers/Energy ratios/StageSupport.m
@@ -112,21 +112,21 @@ if ~isempty(IR) && ~isempty(fs) && ~isempty(startthresh) && ~isempty(bpo) && ~is
     if bpo == 3
         bandfc = [100,125,160,200,250,315,400,500,630,800,1000,1250,1600, ...
             2000,2500,3150,4000,5000];
-        All_IR = thirdoctbandfilter(IR,fs,bandfc); % IR
-        Direct10 = thirdoctbandfilter(Direct10,fs,bandfc); 
-        Early20_100 = thirdoctbandfilter(Early20_100,fs,bandfc);
-        Late100_200 = thirdoctbandfilter(Late100_200,fs,bandfc);
-        All20_1000 = thirdoctbandfilter(All20_1000,fs,bandfc);
-        Late100_1000 = thirdoctbandfilter(Late100_1000,fs,bandfc);
+        All_IR = thirdoctbandfilter_viaFFT(IR,fs,bandfc,15,round(0.1*fs)); % IR
+        Direct10 = thirdoctbandfilter_viaFFT(Direct10,fs,bandfc,15,round(0.1*fs)); 
+        Early20_100 = thirdoctbandfilter_viaFFT(Early20_100,fs,bandfc,15,round(0.1*fs));
+        Late100_200 = thirdoctbandfilter_viaFFT(Late100_200,fs,bandfc,15,round(0.1*fs));
+        All20_1000 = thirdoctbandfilter_viaFFT(All20_1000,fs,bandfc,15,round(0.1*fs));
+        Late100_1000 = thirdoctbandfilter_viaFFT(Late100_1000,fs,bandfc,15,round(0.1*fs));
 
     else
         bandfc = [125,250,500,1000,2000,4000];
-        All_IR = octbandfilter(IR,fs,bandfc); % IR
-        Direct10 = octbandfilter(Direct10,fs,bandfc); 
-        Early20_100 = octbandfilter(Early20_100,fs,bandfc);
-        Late100_200 = octbandfilter(Late100_200,fs,bandfc);
-        All20_1000 = octbandfilter(All20_1000,fs,bandfc);
-        Late100_1000 = octbandfilter(Late100_1000,fs,bandfc);
+        All_IR = octbandfilter_viaFFT(IR,fs,bandfc,12,round(0.1*fs)); % IR
+        Direct10 = octbandfilter_viaFFT(Direct10,fs,bandfc,12,round(0.1*fs)); 
+        Early20_100 = octbandfilter_viaFFT(Early20_100,fs,bandfc,12,round(0.1*fs));
+        Late100_200 = octbandfilter_viaFFT(Late100_200,fs,bandfc,12,round(0.1*fs));
+        All20_1000 = octbandfilter_viaFFT(All20_1000,fs,bandfc,12,round(0.1*fs));
+        Late100_1000 = octbandfilter_viaFFT(Late100_1000,fs,bandfc,12,round(0.1*fs));
 
     end
 
@@ -141,8 +141,8 @@ if ~isempty(IR) && ~isempty(fs) && ~isempty(startthresh) && ~isempty(bpo) && ~is
     Late100_1000 = permute(sum(Late100_1000.^2),[3,2,1]);
 
     ST1 = 10*log10(Early20_100 ./ Direct10); 
-    ST2 = 10*log10(Late100_200 ./ Direct10); % C80
-    STLate = (Late100_1000 ./ Direct10); % D50
+    ST2 = 10*log10(Late100_200 ./ Direct10); 
+    STLate = (Late100_1000 ./ Direct10); 
 
     meanbands = find(bandfc>=200 & bandfc<=2500);
     ST1av = mean(ST1(meanbands,:),1);
@@ -151,54 +151,146 @@ if ~isempty(IR) && ~isempty(fs) && ~isempty(startthresh) && ~isempty(bpo) && ~is
 
 
     %--------------------------------------------------------------------------
-    % OUTPUT STRUCTURE
+    % OUTPUT STRUCTURE (not used anymore)
     %--------------------------------------------------------------------------
 
-    if chans == 1
-        out.ST1 = [bandfc;ST1'];
-        out.ST2 = [bandfc;ST2'];
-        out.STLate = [bandfc;STLate'];
-        out.funcallback.name = 'StageSupport.m';
-        out.funcallback.inarg = {fs,startthresh,bpo};
+%     if chans == 1
+%         out.ST1 = [bandfc;ST1'];
+%         out.ST2 = [bandfc;ST2'];
+%         out.STLate = [bandfc;STLate'];
+%         out.funcallback.name = 'StageSupport.m';
+%         out.funcallback.inarg = {fs,startthresh,bpo};
+% 
+%     else %chans == 2
+%         out.ST1_ch1 = [bandfc;ST1(:,1)'];
+%         out.ST1_ch2 = [bandfc;ST1(:,2)'];
+% 
+%         out.ST2_ch1 = [bandfc;ST2(:,1)'];
+%         out.ST2_ch2 = [bandfc;ST2(:,2)'];
+% 
+%         out.STLate_ch1 = [bandfc;STLate(:,1)'];
+%         out.STLate_ch2 = [bandfc;STLate(:,2)'];
+% 
+%         out.funcallback.name = 'StageSupport.m';
+%         out.funcallback.inarg = {fs,startthresh,bpo};
+%     end % if chans
 
-    else %chans == 2
-        out.ST1_ch1 = [bandfc;ST1(:,1)'];
-        out.ST1_ch2 = [bandfc;ST1(:,2)'];
-
-        out.ST2_ch1 = [bandfc;ST2(:,1)'];
-        out.ST2_ch2 = [bandfc;ST2(:,2)'];
-
-        out.STLate_ch1 = [bandfc;STLate(:,1)'];
-        out.STLate_ch2 = [bandfc;STLate(:,2)'];
-
-        out.funcallback.name = 'StageSupport.m';
-        out.funcallback.inarg = {fs,startthresh,bpo};
-    end % if chans
-
+    out.funcallback.name = 'StageSupport.m';
+    out.funcallback.inarg = {fs,startthresh,bpo};
 
     % OUTPUT TABLE
-
+    
+    
+    out.tables = [];
+    for ch = 1:chans
+        f = figure('Name',['Stage Support, channel: ', num2str(ch)], ...
+            'Position',[200 200 620 360]);
+        %[left bottom width height]
+        dat1 = [ST1(:,ch)';ST2(:,ch)';STLate(:,ch)'];
+        cnames1 = num2cell(bandfc);
+        rnames1 = {'ST1 or ST Early (dB)', 'ST2 (dB)', 'ST Late (dB)'};
+        t1 =uitable('Data',dat1,'ColumnName',cnames1,'RowName',rnames1);
+        set(t1,'ColumnWidth',{60});
+        
+        dat2 = [ST1av(:,ch)';ST2av(:,ch)';STLateav(:,ch)'];
+        cnames2 = {'Spectral Average (250 Hz - 2kHz Octave Bands)'};
+        rnames2 = {'ST1 or ST Early (dB)','ST2 (dB)', ...
+            'ST Late (dB)'};
+        t2 =uitable('Data',dat2,'ColumnName',cnames2,'RowName',rnames2);
+        
+        [~,tables] = disptables(f,[t2 t1],{['Chan' num2str(ch) ' - Spectral average'],['Chan' num2str(ch) ' - Stage support']});
+        out.tables = [out.tables tables];
+    end
     if doplot
-        out.tables = [];
+            ymax = 10*ceil(max(max(ST1+5))/10);
+            ymin = 10*floor(min(min([ST2,STLate]))/10);
         for ch = 1:chans
-            f = figure('Name',['Stage Support, channel: ', num2str(ch)], ...
-                        'Position',[200 200 620 360]);
-            %[left bottom width height]
-            dat1 = [ST1(:,ch)';ST2(:,ch)';STLate(:,ch)'];
-            cnames1 = num2cell(bandfc);
-            rnames1 = {'ST1 or ST Early (dB)', 'ST2 (dB)', 'ST Late (dB)'};
-            t1 =uitable('Data',dat1,'ColumnName',cnames1,'RowName',rnames1);
-            set(t1,'ColumnWidth',{60});
+            figure('name', ['Stage Support, Channel ', ...
+                num2str(ch)])
+            
+            
+            subplot(3,1,1)
+            width = 0.5;
+            bar(1:length(bandfc),ST1(:,ch),width,'FaceColor',[1,0.3,0.3],...
+                'EdgeColor',[0,0,0],'DisplayName', 'STEarly','BaseValue',ymin);
+            hold on
 
-            dat2 = [ST1av(:,ch)';ST2av(:,ch)';STLateav(:,ch)'];
-            cnames2 = {'Spectral Average (250 Hz - 2kHz Octave Bands)'};
-            rnames2 = {'ST1 or ST Early (dB)','ST2 (dB)', ...
-                'ST Late (dB)'};
-            t2 =uitable('Data',dat2,'ColumnName',cnames2,'RowName',rnames2);
+            % x-axis
+            set(gca,'XTick',1:length(bandfc),'XTickLabel',num2cell(bandfc))
+            if bpo==3
+                xlabel('1/3-Octave Band Centre Frequency (Hz)')
+            else
+                xlabel('Octave Band Centre Frequency (Hz)')
+            end
 
-            [~,tables] = disptables(f,[t2 t1],{['Chan' num2str(ch) ' - Spectral average'],['Chan' num2str(ch) ' - Stage support']});
-            out.tables = [out.tables tables];
+            % y-axis
+            ylabel('ST Early (dB)')
+            ylim([ymin ymax])
+
+            legend 'off'
+
+            for k = 1:length(bandfc)
+                text(k-0.25,ymin+(ymax-ymin)*0.025, ...
+                    num2str(round(ST1(k,ch)*10)/10),'Color',[1,0.3,0.3])
+            end
+            
+            
+            
+            subplot(3,1,2)
+            width = 0.5;
+            bar(1:length(bandfc),ST2(:,ch),width,'FaceColor',[0.3,1,0.3],...
+                'EdgeColor',[0,0,0],'DisplayName', 'ST2','BaseValue',ymin);
+            hold on
+
+            % x-axis
+            set(gca,'XTick',1:length(bandfc),'XTickLabel',num2cell(bandfc))
+            if bpo==3
+                xlabel('1/3-Octave Band Centre Frequency (Hz)')
+            else
+                xlabel('Octave Band Centre Frequency (Hz)')
+            end
+
+            % y-axis
+            ylabel('ST2 (dB)')
+            ylim([ymin ymax])
+
+            legend 'off'
+
+            for k = 1:length(bandfc)
+                text(k-0.25,ymin+(ymax-ymin)*0.025, ...
+                    num2str(round(ST2(k,ch)*10)/10),'Color',[0.3,1,0.3])
+            end
+            
+            
+            
+            subplot(3,1,3)
+            width = 0.5;
+            bar(1:length(bandfc),STLate(:,ch),width,'FaceColor',[0.3,0.3,1],...
+                'EdgeColor',[0,0,0],'DisplayName', 'STLate','BaseValue',ymin);
+            hold on
+
+            % x-axis
+            set(gca,'XTick',1:length(bandfc),'XTickLabel',num2cell(bandfc))
+            if bpo==3
+                xlabel('1/3-Octave Band Centre Frequency (Hz)')
+            else
+                xlabel('Octave Band Centre Frequency (Hz)')
+            end
+
+            % y-axis
+            ylabel('ST Late (dB)')
+            ylim([ymin ymax])
+
+            legend 'off'
+
+            for k = 1:length(bandfc)
+                text(k-0.25,ymin+(ymax-ymin)*0.025, ...
+                    num2str(round(STLate(k,ch)*10)/10),'Color',[0.3,0.3,1])
+            end
+            
+            
         end
+        
     end
 else
     out = [];
