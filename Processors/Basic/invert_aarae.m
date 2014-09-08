e3981c0c6d6ba363d04af5842382a0ada84a8e40
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-09-08 23:28:27 +0000

    * added audiofield_extract basic processor. * minor comment change in invert_aarae processor

--- Processors/Basic/invert_aarae.m
+++ Processors/Basic/invert_aarae.m
@@ -1,5 +1,5 @@
 function out = invert_aarae(in)
-% The input audio waveform is multiplied by -1
+% The input audio waveform is multiplied by -1 (i.e. inverted, or phase-reversed)
     out.audio = in.audio;
     indices = partial_selection(in);
     out.audio(indices{:}) = -in.audio(indices{:});
