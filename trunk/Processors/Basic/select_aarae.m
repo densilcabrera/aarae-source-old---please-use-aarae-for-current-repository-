1c4b750381576998f041e7c6843dfa94c82e2a81
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-09-09 12:40:27 +0000

    some corrections to select_aarae

--- trunk/Processors/Basic/select_aarae.m
+++ trunk/Processors/Basic/select_aarae.m
@@ -8,7 +8,7 @@ function out = select_aarae(in)
     indices = partial_selection(in);    
     out.audio(indices{:}) = in.audio(indices{:});
        
-    if isfield('in','cal') && length(indices)>1
+    if isfield(in,'cal') && length(indices)>1
         if length(in.cal) == size(in.audio,2)
             out.cal = in.cal(indices{2});
         else
@@ -16,7 +16,7 @@ function out = select_aarae(in)
         end      
     end
     
-    if isfield('in','chanID') && length(indices)>1
+    if isfield(in,'chanID') && length(indices)>1
         if length(in.chanID) == size(in.audio,2)
             out.chanID = in.chanID(indices{2});
         else
@@ -25,15 +25,16 @@ function out = select_aarae(in)
     end
     
     
-    if isfield('in','bandID') && length(indices)>2
+    if isfield(in,'bandID') && length(indices)>2
         if length(in.bandID) == size(in.audio,3)
-            out.bandID = in.bandID(indices{2});
+            out.bandID = in.bandID(indices{3});
         else
             out.bandID = [];
         end      
     end
     
     % potentially include other fields for similar treatment
+
             
     out.funcallback.name = 'select_aarae.m';
     out.funcallback.inarg = {};
