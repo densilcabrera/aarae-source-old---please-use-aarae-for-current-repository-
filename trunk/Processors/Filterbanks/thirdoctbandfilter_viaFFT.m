04d81c6761561d612bbfc19b379e8f95be8dc4cc
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-09-24 03:07:26 +0000

    some memory management improvements to octbandfilterviaFFT etc

--- trunk/Processors/Filterbanks/thirdoctbandfilter_viaFFT.m
+++ trunk/Processors/Filterbanks/thirdoctbandfilter_viaFFT.m
@@ -191,7 +191,7 @@ if ok == 1
         fftlen = minfftlen;
     end
     
-    spectrum = fft(audio, fftlen);
+    audio = fft(audio, fftlen);
     if test == 1
         testspectrum = fft(testaudio, fftlen);
     end
@@ -262,18 +262,19 @@ if ok == 1
         end
         
         if (phasemode == 0) || (phasemode == 10)
-            bandfiltered = ifft(repmat(mag,[1,chans,1,dim4,dim5,dim6]) .* spectrum);
+            bandfiltered = ifft(repmat(mag,[1,chans,1,dim4,dim5,dim6]) .* audio);
         elseif (phasemode == -1) || (phasemode == 1)
              % real output only for min phase and max phase
-             bandfiltered = real(ifft(repmat(mag,[1,chans,1,dim4,dim5,dim6]) .* spectrum));
+             bandfiltered = real(ifft(repmat(mag,[1,chans,1,dim4,dim5,dim6]) .* audio));
         elseif (phasemode == -11) || (phasemode == 11)
             % quadrature min and max phase
-             bandfiltered = ifft(repmat(mag,[1,chans,1,dim4,dim5,dim6]) .* spectrum);
+             bandfiltered = ifft(repmat(mag,[1,chans,1,dim4,dim5,dim6]) .* audio);
         else
             disp('Phasemode value not recognized');
             OUT = [];
             return
         end
+        
         
         % truncate waveform and send to filtered waveform matrix
         filtered(:,:,b,:,:,:) = bandfiltered(1:len,:,1,:,:,:);
@@ -284,6 +285,7 @@ if ok == 1
         end
         
     end
+    clear audio bandfiltered mag testaudiofiltered
     
     if test == 1
         testlevel = 10*log10(abs(fft(testaudioout)).^2);
