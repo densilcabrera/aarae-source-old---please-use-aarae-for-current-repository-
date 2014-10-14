085be7dec605f64e2695f4e7d28888b5060d40b1
densil.cabrera <densil.cabrera@fc00c1c8-37ab-558f-bb42-9189f7ac81b3>
2014-10-14 04:22:50 +0000

    makechanID in analyser template

--- Templates/analyser_template.m
+++ Templates/analyser_template.m
@@ -105,7 +105,8 @@ if isstruct(IN) % You should check that the function is being called within
     if isfield(IN,'chanID') % Get the channel ID if it exists
         chanID = IN.chanID;
     else
-        chanID = cellstr([repmat('Chan',size(audio,2),1) num2str((1:size(audio,2))')]);
+        % or make a chanID using AARAE's utility function
+        chanID = makechanID(size(audio,2),0);
     end
     
     % bandID is a vector, usually listing the centre frequencies of the
