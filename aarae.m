388d837ca3109addcca3d5ab5f3e34a7d2c8e17b
densil <densil.cabrera@sydney.edu.au>
2015-08-02 21:14:03 +1000

    cal bug fixed in audio comparison plots

--- aarae.m
+++ aarae.m
@@ -1956,6 +1956,15 @@ if handles.compareaudio == 1
             signaldata.audio = signaldata.audio2;
         end
         if ~isempty(signaldata) && isfield(signaldata,'audio')
+            if isfield(signaldata,'cal') && handles.Settings.calibrationtoggle == 1
+                if size(signaldata.audio,2) == length(signaldata.cal)
+                    signaldata.cal(isnan(signaldata.cal)) = 0;
+                    signaldata.audio = signaldata.audio .* ...
+                        repmat(10.^(signaldata.cal(:)'./20),[size(signaldata.audio,1),1,...
+                        size(signaldata.audio,3),size(signaldata.audio,4),...
+                        size(signaldata.audio,5),size(signaldata.audio,6)]);
+                end
+            end
             To = floor(str2double(get(handles.To_time,'String'))*signaldata.fs)+1;
             Tf = floor(str2double(get(handles.Tf_time,'String'))*signaldata.fs);
             if Tf > length(signaldata.audio), Tf = length(signaldata.audio); end
@@ -1967,14 +1976,6 @@ if handles.compareaudio == 1
                 dim6plot(dim6plot<=dim6(i)));
             t = linspace(0,length(signaldata.audio),length(signaldata.audio))./signaldata.fs;
             f = signaldata.fs .* ((1:length(signaldata.audio))-1) ./ length(signaldata.audio);
-%             if isfield(signaldata,'cal') && handles.Settings.calibrationtoggle == 1
-%                 if size(chans(i),2) == length(signaldata.cal)
-%                     signaldata.cal(isnan(signaldata.cal)) = 0;
-%                     signaldata.cal = signaldata.cal(chanplot(i));
-%                     signaldata.audio = signaldata.audio.*repmat(10.^(signaldata.cal(:)'./20),...
-%                         [len(i),1,bandsselect,cyclesselect,outchansselect,dim6select]);
-%                 end
-%             end
             switch handles.Settings.specmagscale;
                 case {'Divided by length'}
                     spectscale = 1./len(i);
