40f3a88b4d4094858335f33e11151afde74d47be
densil <densil.cabrera@sydney.edu.au>
2015-10-24 12:45:59 +1100

    further refinements to RoughnesDW (no change in functionality)

--- Analysers/Level and Strength/RoughnessDW.m
+++ Analysers/Level and Strength/RoughnessDW.m
@@ -3,8 +3,15 @@ function OUT = RoughnessDW(IN,ov)
 % roughness using the roughness model by Daniel & Weber:
 % Daniel, P., & Weber, R. (1997). Psychoacoustical roughness: implementation 
 % of an optimized model. Acustica(83), 113-123.
-% reference signal: 60dB 1kHz tone 100% modulated at 70Hz would output 1
+%
+% Reference signal: 60dB 1kHz tone 100% modulated at 70 Hz should output 1
 % asper.
+%
+% This is a port of the RoughnessDW analyser from PsySound3. The original
+% code for this implementation was provided by Dik Hermes. It was adapted
+% for PsySound3 by Matt Flax (2006), with further changes by Farhan Rizwi
+% (2007). It was ported to AARAE by Ella Manor and Densil Cabrera (2015).
+%
 
 % INPUT ARGUMENTS
 % IN - audio signal (1 channel only)
@@ -20,9 +27,6 @@ function OUT = RoughnessDW(IN,ov)
 % * Time-averaged Specific Roughness
 % * Time-averaged Roughness statistics
 
-% Author: Matt Flax (http://www.flatmax.org>) original version: March 2006 for psysound
-% revised: Farhan Rizwi: July '07 for psysound3
-% Adjusted version by Ella Manor for AARAE 23-10-2015
 
 % *************************************************************************
 
@@ -566,7 +570,9 @@ ri_mat = zeros(47,n);
         endIndex = endIndex+N;
         TimePoints(windowNum,1) = currentTimePoint;
     end
-
+else
+    OUT = [];
+    return
 end
 
 %%%%%%%%%%%%%%%%%%%%%%
@@ -683,32 +689,3 @@ end
 
 
 
-%**************************************************************************
-% Copyright (c) <YEAR>, <OWNER>
-% All rights reserved.
-%
-% Redistribution and use in source and binary forms, with or without 
-% modification, are permitted provided that the following conditions are 
-% met:
-%
-%  * Redistributions of source code must retain the above copyright notice,
-%    this list of conditions and the following disclaimer.
-%  * Redistributions in binary form must reproduce the above copyright 
-%    notice, this list of conditions and the following disclaimer in the 
-%    documentation and/or other materials provided with the distribution.
-%  * Neither the name of the <ORGANISATION> nor the names of its contributors
-%    may be used to endorse or promote products derived from this software 
-%    without specific prior written permission.
-%
-% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-% TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
-% PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
-% OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-% EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-% PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-% PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-% LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
-% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-%**************************************************************************
